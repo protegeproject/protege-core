efcdce41142394e05e678dc1fc2c0bba49d7e57b
fergerson <fergerson@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2005-03-30 23:28:10 +0000

    getFrame performance improvement on database backend

--- protege-core/trunk/src/edu/stanford/smi/protege/storage/database/DatabaseFrameStore.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/storage/database/DatabaseFrameStore.java
@@ -1,24 +1,19 @@
 package edu.stanford.smi.protege.storage.database;
 
 import java.sql.*;
+import java.util.*;
 
 import edu.stanford.smi.protege.model.*;
 import edu.stanford.smi.protege.model.framestore.*;
 
 public class DatabaseFrameStore extends SimpleFrameStore {
     private DatabaseFrameDb _databaseFrameDb;
+    private Map nameToFrameMap = new HashMap();
 
-    public DatabaseFrameStore(
-        DefaultKnowledgeBase kb,
-        String driver,
-        String url,
-        String username,
-        String password,
-        String tablename,
-        boolean modifiable) {
-        super(
-            kb,
-            getBasicFrameStore(new DatabaseFrameDb(getFrameFactory(kb), driver, url, username, password, tablename)));
+    public DatabaseFrameStore(DefaultKnowledgeBase kb, String driver, String url, String username, String password,
+            String tablename, boolean modifiable) {
+        super(kb, getBasicFrameStore(new DatabaseFrameDb(getFrameFactory(kb), driver, url, username, password,
+                tablename)));
         _databaseFrameDb = getDatabaseFrameDb();
         _databaseFrameDb.setModifiable(modifiable);
         // addSystemFrames();
@@ -55,6 +50,24 @@ public class DatabaseFrameStore extends SimpleFrameStore {
 
     public String getTableName() {
         return getDatabaseFrameDb().getTableName();
+    }
+
+    public Frame getFrame(String name) {
+        Frame frame = (Frame) nameToFrameMap.get(name);
+        if (!isGood(frame, name)) {
+            frame = super.getFrame(name);
+            nameToFrameMap.put(name, frame);
+        }
+        return frame;
+    }
+
+    public void setFrameName(Frame frame, String name) {
+        super.setFrameName(frame, name);
+        nameToFrameMap.put(name, frame);
+    }
+
+    private static boolean isGood(Frame frame, String name) {
+        return frame != null && !frame.isDeleted() && frame.getName().equals(name);
     }
 
 }
