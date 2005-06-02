ea4992153b0eaff7e4dbd47b935b0624eefd17d9
fergerson <fergerson@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2005-06-02 21:45:57 +0000

    XML included frame bug

--- protege-core/trunk/src/edu/stanford/smi/protege/storage/xml/XMLLoader.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/storage/xml/XMLLoader.java
@@ -19,11 +19,13 @@ public class XMLLoader {
     private KnowledgeBase kb;
     private Collection errors;
     private BufferedReader reader;
+    private boolean isIncluded;
 
-    public XMLLoader(KnowledgeBase kb, BufferedReader reader, boolean isInclude, Collection errors) {
+    public XMLLoader(KnowledgeBase kb, BufferedReader reader, boolean isIncluded, Collection errors) {
         this.kb = kb;
         this.reader = reader;
         this.errors = errors;
+        this.isIncluded = isIncluded;
     }
 
     public void load() {
@@ -31,7 +33,7 @@ public class XMLLoader {
             SAXParserFactory factory = SAXParserFactory.newInstance();
             factory.setNamespaceAware(true);
             SAXParser saxParser = factory.newSAXParser();
-            saxParser.parse(new InputSource(reader), new XMLHandler(kb, errors));
+            saxParser.parse(new InputSource(reader), new XMLHandler(kb, isIncluded, errors));
         } catch (Exception e) {
             Log.getLogger().severe(Log.toString(e));
         }
