d79d8a34605997001265fe8ef6df4733c93a2814
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-01-02 04:03:51 +0000

    Organized Imports.

--- src/edu/stanford/smi/protege/model/framestore/FrameStore.java
+++ src/edu/stanford/smi/protege/model/framestore/FrameStore.java
@@ -1,9 +1,18 @@
 package edu.stanford.smi.protege.model.framestore;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
-import edu.stanford.smi.protege.model.*;
-import edu.stanford.smi.protege.model.query.*;
+import edu.stanford.smi.protege.model.Cls;
+import edu.stanford.smi.protege.model.Facet;
+import edu.stanford.smi.protege.model.Frame;
+import edu.stanford.smi.protege.model.FrameID;
+import edu.stanford.smi.protege.model.Instance;
+import edu.stanford.smi.protege.model.KnowledgeBase;
+import edu.stanford.smi.protege.model.SimpleInstance;
+import edu.stanford.smi.protege.model.Slot;
+import edu.stanford.smi.protege.model.query.Query;
 
 public interface FrameStore {
 
