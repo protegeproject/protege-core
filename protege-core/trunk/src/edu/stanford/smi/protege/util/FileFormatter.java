8fe95adcb4f3f58690b789eb3227f28e13c6635e
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-01-02 04:25:20 +0000

    Organized imports.

--- protege-core/trunk/src/edu/stanford/smi/protege/util/FileFormatter.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/util/FileFormatter.java
@@ -1,6 +1,6 @@
 package edu.stanford.smi.protege.util;
 
-import java.util.logging.*;
+import java.util.logging.LogRecord;
 
 /**
  * TODO Class Comment
@@ -8,6 +8,10 @@ import java.util.logging.*;
  * @author Ray Fergerson <fergerson@smi.stanford.edu>
  */
 public class FileFormatter extends AbstractFormatter {
+  
+  public FileFormatter() {
+    // System.out.println("Constructing file formatter");
+  }
     
     public String format(LogRecord record) {
         return format(record, null, true, true);
