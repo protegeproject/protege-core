76a9dbf3e04d1a78bab98aa19ea3ca097b45500a
Tania Tudorache <tudorache@users.noreply.github.com>
2006-02-22 23:41:13 +0000

    Updated to work with new version of unicode_panel.jar (ver 1.1.7)

--- src/edu/stanford/smi/protege/action/InsertUnicodeCharacterAction.java
+++ src/edu/stanford/smi/protege/action/InsertUnicodeCharacterAction.java
@@ -2,6 +2,8 @@ package edu.stanford.smi.protege.action;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 import javax.swing.*;
 import javax.swing.text.*;
@@ -14,6 +16,7 @@ import edu.stanford.smi.protege.util.*;
 /**
  * 
  * @author Ray Fergerson <fergerson@smi.stanford.edu>
+ * @author Tania Tudorache <tudorache@stanford.edu>
  */
 public class InsertUnicodeCharacterAction extends TextAction {
     private UnicodeChooser chooser;
@@ -24,30 +27,38 @@ public class InsertUnicodeCharacterAction extends TextAction {
     }
 
     public void actionPerformed(ActionEvent event) {
-        JTextComponent textComponent = getTextComponent(event);
+        final JTextComponent textComponent = getTextComponent(event);
         if (textComponent != null) {
 	        if (chooser == null) {
 	            Window window = SwingUtilities.windowForComponent(textComponent);
 	            if (window == null) {
-	                Log.getLogger().warning("no window");
-	            } else if (window instanceof JDialog) {
-	                chooser = new UnicodeChooser((Dialog) window);
-	            } else if (window instanceof Frame) {
-	                chooser = new UnicodeChooser((Frame) window);
-	            } else {
-	                Log.getLogger().warning("unknown window type " + window);
-	            }
-	        }
-	        chooser.setVisible(true);
-	        Character symbol = chooser.getSelectedCharacter();
-	        if (symbol != null) {
+	                Log.getLogger().warning("no window for unicode panel");
+	            } 
 	            try {
-	                int pos = textComponent.getCaretPosition();
-	                textComponent.getDocument().insertString(pos, symbol.toString(), null);
-	            } catch (BadLocationException e) {
-	                // do nothing
-	            }
+	            	chooser = UnicodeChooser.instance(window);	
+				} catch (RuntimeException e) {
+					Log.getLogger().warning(e.getMessage());
+				}	                        
 	        }
+	        
+	        chooser.setVisible(true);
+	        
+			chooser.addPropertyChangeListener(new PropertyChangeListener() {
+				public void propertyChange(PropertyChangeEvent evt) {
+					// This check is currently not necessary as only one property change occurs,
+					// but it is good practice anyway
+					if (UnicodeChooser.INSERT_CHARACTER_PROPERTY.equals(evt.getPropertyName())) {
+						Character symbol = (Character) evt.getNewValue();
+						if (symbol != null) {
+							try {
+								textComponent.getDocument().insertString(textComponent.getCaretPosition(), symbol.toString(), null);
+							} catch (BadLocationException e) {								
+								 // do nothing
+							}
+						}
+					}
+				}
+	        });	    
         }
     }
 }
