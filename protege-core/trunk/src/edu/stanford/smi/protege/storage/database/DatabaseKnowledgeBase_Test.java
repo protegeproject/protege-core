c60ee53964489a285d2e70cb63bd707f66874a15
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-02-24 01:10:57 +0000

    Modified this test so that only the configured tests run.  Discovered mysql 5.0 problem is back?

--- protege-core/trunk/src/edu/stanford/smi/protege/storage/database/DatabaseKnowledgeBase_Test.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/storage/database/DatabaseKnowledgeBase_Test.java
@@ -3,7 +3,6 @@ package edu.stanford.smi.protege.storage.database;
 import java.util.Collection;
 
 import edu.stanford.smi.protege.model.Cls;
-import edu.stanford.smi.protege.model.DefaultKnowledgeBase;
 import edu.stanford.smi.protege.model.DefaultKnowledgeBase_Test;
 import edu.stanford.smi.protege.model.Instance;
 import edu.stanford.smi.protege.model.KnowledgeBase;
@@ -15,48 +14,66 @@ import edu.stanford.smi.protege.test.APITestCase;
 public class DatabaseKnowledgeBase_Test extends APITestCase {
   
   public void testDBModficationSlots() throws java.text.ParseException {
-    DefaultKnowledgeBase_Test dkbt = new DefaultKnowledgeBase_Test();
-    DefaultKnowledgeBase_Test.setDatabaseProject();
-    dkbt.testModificationSlots();
+    for (DBType dbt : DBType.values()) {
+      setDBType(dbt);
+      if (!dbConfigured()) {
+        continue;
+      }
+      DefaultKnowledgeBase_Test dkbt = new DefaultKnowledgeBase_Test();
+      DefaultKnowledgeBase_Test.setDatabaseProject();
+      dkbt.testModificationSlots();
+    }
   }
 
   public void testGetDBFramesWithValue() {
-    setDatabaseProject();
-    Slot s1 = createMultiValuedSlot(ValueType.STRING);
-    Slot s2 = createMultiValuedSlot(ValueType.INSTANCE);
-    Cls a = createCls();
-    a.addDirectTemplateSlot(s1);
-    a.addDirectTemplateSlot(s2);
-    Instance inst1 = createInstance(a);
-    Instance inst2 = createInstance(a);
-    Instance inst3 = createInstance(a);
-
-    inst1.addOwnSlotValue(s1, "abc");
-    inst2.addOwnSlotValue(s1, "abc");
-    inst3.addOwnSlotValue(s1, "abc");
-
-    inst2.addOwnSlotValue(s2, inst1);
-
-    assertEquals("string match", 3, getDomainKB().getFramesWithValue(s1, null, false, "abc").size());
-    assertEquals("frame match", 1, getDomainKB().getFramesWithValue(s2, null, false, inst1).size());
+    for (DBType dbt : DBType.values()) {
+      setDBType(dbt);
+      if (!dbConfigured()) {
+        continue;
+      }
+      setDatabaseProject();
+      Slot s1 = createMultiValuedSlot(ValueType.STRING);
+      Slot s2 = createMultiValuedSlot(ValueType.INSTANCE);
+      Cls a = createCls();
+      a.addDirectTemplateSlot(s1);
+      a.addDirectTemplateSlot(s2);
+      Instance inst1 = createInstance(a);
+      Instance inst2 = createInstance(a);
+      Instance inst3 = createInstance(a);
+      
+      inst1.addOwnSlotValue(s1, "abc");
+      inst2.addOwnSlotValue(s1, "abc");
+      inst3.addOwnSlotValue(s1, "abc");
+      
+      inst2.addOwnSlotValue(s2, inst1);
+      
+      assertEquals("string match", 3, getDomainKB().getFramesWithValue(s1, null, false, "abc").size());
+      assertEquals("frame match", 1, getDomainKB().getFramesWithValue(s2, null, false, inst1).size());
+    }
   }
   
   public void testGetMatchingDBReferences() {
-    setDatabaseProject();
-    Slot s = createMultiValuedSlot(ValueType.STRING);
-    Cls a = createCls();
-    a.addDirectTemplateSlot(s);
-    Instance inst1 = createInstance(a);
-    inst1.addOwnSlotValue(s, "zabcy");
-    Instance inst2 = createInstance(a);
-    inst2.addOwnSlotValue(s, "abcz");
-    Instance inst3 = createInstance(a);
-    inst3.addOwnSlotValue(s, "qqq");
-    assertEquals("exact", 0, getMatchCount("z"));
-    assertEquals("starts", 1, getMatchCount("z*"));
-    assertEquals("contains", 2, getMatchCount("*z*"));
-    assertEquals("contains insensitive", 2, getMatchCount("*Z*"));
-    assertEquals("contains 2", 2, getMatchCount("*abc*"));
+    for (DBType dbt : DBType.values()) {
+      setDBType(dbt);
+      if (!dbConfigured()) {
+        continue;
+      }
+      setDatabaseProject();
+      Slot s = createMultiValuedSlot(ValueType.STRING);
+      Cls a = createCls();
+      a.addDirectTemplateSlot(s);
+      Instance inst1 = createInstance(a);
+      inst1.addOwnSlotValue(s, "zabcy");
+      Instance inst2 = createInstance(a);
+      inst2.addOwnSlotValue(s, "abcz");
+      Instance inst3 = createInstance(a);
+      inst3.addOwnSlotValue(s, "qqq");
+      assertEquals("exact", 0, getMatchCount("z"));
+      assertEquals("starts", 1, getMatchCount("z*"));
+      assertEquals("contains", 2, getMatchCount("*z*"));
+      assertEquals("contains insensitive", 2, getMatchCount("*Z*"));
+      assertEquals("contains 2", 2, getMatchCount("*abc*"));
+    }
   }
   
   
@@ -65,48 +82,66 @@ public class DatabaseKnowledgeBase_Test extends APITestCase {
   }
 
   public void testMatchOnPercentInDB() {
-    setDatabaseProject();
-    String name1 = "foo %";
-    String name2 = "foo abc";
-    Slot nameSlot = getDomainKB().getSlot(Model.Slot.NAME);
+    for (DBType dbt : DBType.values()) {
+      setDBType(dbt);
+      if (!dbConfigured()) {
+        continue;
+      }
 
-    createCls(name1);
-    createCls(name2);
+      setDatabaseProject();
+      String name1 = "foo %";
+      String name2 = "foo abc";
+      Slot nameSlot = getDomainKB().getSlot(Model.Slot.NAME);
 
-    Collection frames = getDomainKB().getMatchingFrames(nameSlot, null, false, name1, -1);
-    assertEquals("matching size", 1, frames.size());
+      createCls(name1);
+      createCls(name2);
 
-    frames = getDomainKB().getReferences(name1, -1);
-    assertEquals("references size", 1, frames.size());
+      Collection frames = getDomainKB().getMatchingFrames(nameSlot, null, false, name1, -1);
+      assertEquals("matching size", 1, frames.size());
+
+      frames = getDomainKB().getReferences(name1, -1);
+      assertEquals("references size", 1, frames.size());
+    }
   }
   
   public void testMatchOnQuoteInDB() {
-    setDatabaseProject();
-    String name1 = "foo's";
-    Slot nameSlot = getDomainKB().getSlot(Model.Slot.NAME);
+    for (DBType dbt : DBType.values()) {
+      setDBType(dbt);
+      if (!dbConfigured()) {
+        continue;
+      }
+      setDatabaseProject();
+      String name1 = "foo's";
+      Slot nameSlot = getDomainKB().getSlot(Model.Slot.NAME);
 
-    createCls(name1);
+      createCls(name1);
 
-    Collection frames = getDomainKB().getMatchingFrames(nameSlot, null, false, name1, -1);
-    assertEquals("matching size", 1, frames.size());
+      Collection frames = getDomainKB().getMatchingFrames(nameSlot, null, false, name1, -1);
+      assertEquals("matching size", 1, frames.size());
 
-    frames = getDomainKB().getReferences(name1, -1);
-    assertEquals("references size", 1, frames.size());
+      frames = getDomainKB().getReferences(name1, -1);
+      assertEquals("references size", 1, frames.size());
+    }
   }
   
   public void testGetDBDirectInstances() {
-    setDatabaseProject();
-    Cls cls = createCls();
-    String name = cls.getName();
-    createInstance(cls);
-    createInstance(cls);
-    createInstance(cls);
-    assertEquals("direct instance count", 3, cls.getDirectInstanceCount());
-    saveAndReload();
-    cls = getCls(name);
-    createInstance(cls);
-    assertEquals("direct instance count after reload", 4, cls.getDirectInstanceCount());
+    for (DBType dbt : DBType.values()) {
+      setDBType(dbt);
+      if (!dbConfigured()) {
+        continue;
+      }
+      setDatabaseProject();
+      Cls cls = createCls();
+      String name = cls.getName();
+      createInstance(cls);
+      createInstance(cls);
+      createInstance(cls);
+      assertEquals("direct instance count", 3, cls.getDirectInstanceCount());
+      saveAndReload();
+      cls = getCls(name);
+      createInstance(cls);
+      assertEquals("direct instance count after reload", 4, cls.getDirectInstanceCount());
+    }
   }
-
 
 }
