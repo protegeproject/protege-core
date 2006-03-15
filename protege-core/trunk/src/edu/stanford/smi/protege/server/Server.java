ea619d0521946278cdaf2bfbee23ffc9fe94c5e7
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-03-15 13:30:33 +0000

    Imports.

--- protege-core/trunk/src/edu/stanford/smi/protege/server/Server.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/server/Server.java
@@ -15,6 +15,7 @@ import edu.stanford.smi.protege.model.*;
 import edu.stanford.smi.protege.model.framestore.*;
 import edu.stanford.smi.protege.plugin.*;
 import edu.stanford.smi.protege.resource.*;
+import edu.stanford.smi.protege.server.framestore.LocalizeFrameStoreHandler;
 import edu.stanford.smi.protege.util.*;
 
 public class Server extends UnicastRemoteObject implements RemoteServer {
