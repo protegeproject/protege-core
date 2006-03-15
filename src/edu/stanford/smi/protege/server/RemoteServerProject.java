ed0c11c68d60124e4723677525b6955efdfb3379
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-03-15 13:31:14 +0000

    Minor.

--- src/edu/stanford/smi/protege/server/RemoteServerProject.java
+++ src/edu/stanford/smi/protege/server/RemoteServerProject.java
@@ -1,20 +1,29 @@
 package edu.stanford.smi.protege.server;
 
-import java.net.*;
-import java.rmi.*;
-import java.util.*;
+import java.net.URI;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Collection;
+
+import edu.stanford.smi.protege.server.framestore.RemoteServerFrameStore;
+import edu.stanford.smi.protege.server.narrowframestore.RemoteServerNarrowFrameStore;
 
 public interface RemoteServerProject extends Remote {
     URI getURI(RemoteSession session) throws RemoteException;
 
     RemoteServerFrameStore getDomainKbFrameStore(RemoteSession session) throws RemoteException;
+    
     String getDomainKbFactoryClassName() throws RemoteException;
 
     RemoteServerFrameStore getProjectKbFrameStore(RemoteSession session) throws RemoteException;
+    
     String getProjectKbFactoryClassName() throws RemoteException;
+    
+    RemoteServerNarrowFrameStore getDomainKbNarrowFrameStore() throws RemoteException;
 
     Collection getCurrentSessions(RemoteSession session) throws RemoteException;
 
     void close(RemoteSession session) throws RemoteException;
+    
     void register(RemoteSession session) throws RemoteException;
 }
