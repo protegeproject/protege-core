9a1b2875d23696d971a81a2743dab7d04c6fdbc5
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-04-03 22:15:05 +0000

    Added some debug logs for measuring performance of the server-client interaction.

--- src/edu/stanford/smi/protege/server/framestore/LocalizeFrameStoreHandler.java
+++ src/edu/stanford/smi/protege/server/framestore/LocalizeFrameStoreHandler.java
@@ -2,24 +2,37 @@ package edu.stanford.smi.protege.server.framestore;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import edu.stanford.smi.protege.model.KnowledgeBase;
 import edu.stanford.smi.protege.model.framestore.AbstractFrameStoreInvocationHandler;
 import edu.stanford.smi.protege.model.framestore.FrameStore;
 import edu.stanford.smi.protege.util.LocalizeUtils;
+import edu.stanford.smi.protege.util.Log;
 
 /**
  * 
  * @author Ray Fergerson <fergerson@smi.stanford.edu>
  */
 public class LocalizeFrameStoreHandler extends AbstractFrameStoreInvocationHandler {
+    private static transient Logger log = Log.getLogger(LocalizeFrameStoreHandler.class);
+    private int counter = 0;
+    
     private KnowledgeBase _kb;
 
     public LocalizeFrameStoreHandler(KnowledgeBase kb) {
+        if (log.isLoggable(Level.FINE)) {
+          log.fine("Entering the Localize constructor");
+        }
         _kb = kb;
     }
 
     protected Object handleInvoke(Method method, Object[] args) {
+        if (log.isLoggable(Level.FINE)) {
+          log.fine("Received Remote Invocation of " + method);
+          log.fine("Count = " + (++counter));
+        }
         localize(args);
         return invoke(method, args);
     }
