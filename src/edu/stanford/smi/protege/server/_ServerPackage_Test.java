aec54d9394222ac64b660109e02d4c9fdb6e5323
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-04-03 22:10:55 +0000

    Consolidated some server tests into the same file because the initialization was the same.

--- src/edu/stanford/smi/protege/server/_ServerPackage_Test.java
+++ src/edu/stanford/smi/protege/server/_ServerPackage_Test.java
@@ -12,7 +12,6 @@ public class _ServerPackage_Test {
     public static Test suite() {
         TestSuite suite = new TestSuite("server");
         suite.addTestSuite(Server_Test.class);
-        suite.addTestSuite(RemoteProjectManager_Test.class);
         suite.addTestSuite(RemoteClientFrameStore_Test.class);
         return suite;
     }
