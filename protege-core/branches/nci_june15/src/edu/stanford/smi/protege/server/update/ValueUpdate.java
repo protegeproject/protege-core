5e88b8838b6a74986715998520669b8326bcd3c5
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-04-28 23:23:01 +0000

    Refactor + several changes reflecting the fact that updates to the client cache are no longer represented through events.

---
+++ protege-core/branches/nci_june15/src/edu/stanford/smi/protege/server/update/ValueUpdate.java
@@ -0,0 +1,40 @@
+package edu.stanford.smi.protege.server.update;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import edu.stanford.smi.protege.model.Frame;
+import edu.stanford.smi.protege.model.KnowledgeBase;
+import edu.stanford.smi.protege.model.Localizable;
+import edu.stanford.smi.protege.server.RemoteSession;
+import edu.stanford.smi.protege.util.LocalizeUtils;
+
+public class ValueUpdate implements Localizable, Serializable {
+  private Frame frame;
+  private transient Set<RemoteSession> clients;
+  
+  public ValueUpdate(Frame frame) {
+    this.frame = frame;
+  }
+  
+  public Frame getFrame() {
+    return frame;
+  }
+
+  public Set<RemoteSession> getClients() {
+    return clients;
+  }
+
+  public void setClients(Set<RemoteSession> clients) {
+    this.clients = clients;
+  }
+  
+  public void addClient(RemoteSession client) {
+    clients.add(client);
+  }
+
+  public void localize(KnowledgeBase kb) {
+    LocalizeUtils.localize(frame, kb);
+  }
+
+}
