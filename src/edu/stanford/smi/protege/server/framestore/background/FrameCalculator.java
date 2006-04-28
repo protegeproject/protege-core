b9715fcba4d865ee9efc10d7d79fd9c844b7d371
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-04-28 23:20:50 +0000

    There are changes due to the fact that value updates are now separate from events.  In addition, the frameCalculator will hold its precalculation when some transaction is in progress.

--- src/edu/stanford/smi/protege/server/framestore/background/FrameCalculator.java
+++ src/edu/stanford/smi/protege/server/framestore/background/FrameCalculator.java
@@ -3,7 +3,6 @@ package edu.stanford.smi.protege.server.framestore.background;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.EventObject;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -18,10 +17,31 @@ import edu.stanford.smi.protege.model.Frame;
 import edu.stanford.smi.protege.model.Slot;
 import edu.stanford.smi.protege.model.framestore.FrameStore;
 import edu.stanford.smi.protege.server.RemoteSession;
-import edu.stanford.smi.protege.server.framestore.Registration;
-import edu.stanford.smi.protege.server.framestore.ServerEventWrapper;
+import edu.stanford.smi.protege.server.framestore.ServerFrameStore;
+import edu.stanford.smi.protege.server.update.FrameEvaluationCompleted;
+import edu.stanford.smi.protege.server.update.FrameEvaluationEvent;
+import edu.stanford.smi.protege.server.update.FrameEvaluationStarted;
+import edu.stanford.smi.protege.server.update.ValueUpdate;
 import edu.stanford.smi.protege.server.util.FifoWriter;
+import edu.stanford.smi.protege.util.AbstractEvent;
 import edu.stanford.smi.protege.util.Log;
+
+/*
+ * This frame calculator has to be aware of transactions.  A good
+ * illustrative case is as follows:
+ * 
+ *   client X starts transaction
+ *   client X modifies slot s for frame f
+ *   the frame calculator evaluates a value for slot s for frame f
+ *   client X rolls back the transaction
+ *
+ * At this point the evaluation of the value for the frame f is
+ * invalid.  We will take a simple approach to this situation; we wait
+ * for all transactions to be complete before calculating frame
+ * values.  
+ * 
+ */
+
 
 /**
  * This class is a thread that precalculates frames needed by a client.
@@ -39,8 +59,8 @@ public class FrameCalculator {
   private boolean shuttingDown = false;
   private FrameStore fs;
   private Object kbLock;
-  private FifoWriter<ServerEventWrapper> events;
-  private Map<RemoteSession, Registration> sessionToRegistrationMap;
+  private FifoWriter<ValueUpdate> updates;
+  private ServerFrameStore server;
   
   FrameCalculatorThread innerThread;
   
@@ -54,12 +74,12 @@ public class FrameCalculator {
   
   public FrameCalculator(FrameStore fs,
                          Object kbLock,
-                         FifoWriter<ServerEventWrapper> events,
-                         Map<RemoteSession, Registration> sessionToRegistrationMap) {
+                         FifoWriter<ValueUpdate> updates,
+                         ServerFrameStore server) {
     this.fs = fs;
     this.kbLock = kbLock;
-    this.events = events;
-    this.sessionToRegistrationMap = sessionToRegistrationMap;
+    this.updates = updates;
+    this.server = server;
   }
   
 
@@ -74,10 +94,12 @@ public class FrameCalculator {
       Set<Slot> slots = null;
       List values;
       synchronized (kbLock) {
+        server.waitForTransactionsToComplete();
         slots = fs.getOwnSlots(frame);
       }
       for (Slot slot : slots) {
         synchronized (kbLock) {
+          server.waitForTransactionsToComplete();
           values = fs.getDirectOwnSlotValues(frame, slot);
           insertValueEvent(frame, slot, (Facet) null, false, values);
         }
@@ -85,19 +107,23 @@ public class FrameCalculator {
       if (frame instanceof Cls) {
         Cls cls = (Cls) frame;
         synchronized (kbLock) {
+          server.waitForTransactionsToComplete();
           slots = fs.getTemplateSlots(cls);
         }
         for (Slot slot : slots) {
           synchronized (kbLock) {
+            server.waitForTransactionsToComplete();
             values = fs.getDirectTemplateSlotValues(cls, slot);
           }
           insertValueEvent(cls, slot, (Facet) null, true, values);
           Set<Facet> facets;
           synchronized (kbLock) {
+            server.waitForTransactionsToComplete();
             facets = fs.getTemplateFacets(cls, slot);
           }
           for (Facet facet : facets) {
             synchronized (kbLock) {
+              server.waitForTransactionsToComplete();
               values = fs.getDirectTemplateFacetValues(cls, slot,facet);
               insertValueEvent(cls, slot,  facet, true, values);
             }
@@ -139,6 +165,7 @@ public class FrameCalculator {
       }
       for (Slot slot : slots) {
         synchronized (kbLock) {
+          server.waitForTransactionsToComplete();
           values = fs.getDirectOwnSlotValues(frame, slot);
           insertEvent(
               new FrameEvaluationEvent(frame, slot, (Facet) null, false, values), 
@@ -150,10 +177,12 @@ public class FrameCalculator {
         Cls cls = (Cls) frame;
           
         synchronized (kbLock)  {
+          server.waitForTransactionsToComplete();
           slots = fs.getTemplateSlots(cls);
         }
         for (Slot slot : slots) {
           synchronized (kbLock) {
+            server.waitForTransactionsToComplete();
             values = fs.getDirectTemplateSlotValues(cls, slot);
             insertEvent(
                 new FrameEvaluationEvent(cls, slot, (Facet) null,  true, values), 
@@ -161,10 +190,12 @@ public class FrameCalculator {
           }
           Set<Facet> facets;
           synchronized (kbLock) {
+            server.waitForTransactionsToComplete();
             facets = fs.getTemplateFacets(cls, slot);
           }
           for (Facet facet : facets) {
             synchronized (kbLock) {
+              server.waitForTransactionsToComplete();
               values = fs.getDirectTemplateFacetValues(cls, slot, facet);
               insertEvent(
                   new FrameEvaluationEvent(cls, slot, facet, true, values),
@@ -237,7 +268,7 @@ public class FrameCalculator {
   /*
    * This call assumes that the kbLock is held on entry.
    */
-  private void insertEvent(ClientNotification event) {
+  private void insertEvent(ValueUpdate event) {
     Frame frame = event.getFrame();
     Set<RemoteSession> clients = null;
     synchronized(requestLock) {
@@ -246,15 +277,13 @@ public class FrameCalculator {
     insertEvent(event, clients);
   }
   
-  private void insertEvent(ClientNotification event, Set<RemoteSession> clients) {
-    event.setClients(clients);
-    for (EventObject previousEvent : fs.getEvents()) {
-      events.write(new ServerEventWrapper(previousEvent));
-    }
-    events.write(new ServerEventWrapper(event));
+  private void insertEvent(ValueUpdate update, Set<RemoteSession> clients) {
+    update.setClients(clients);
+    server.updateEvents();
+    updates.write(update);
   }
 
-  public boolean checkInterest(ClientNotification event,
+  public boolean checkInterest(ValueUpdate event,
                                RemoteSession session) {
     synchronized (requestLock) {
       return event.getClients().contains(session);
