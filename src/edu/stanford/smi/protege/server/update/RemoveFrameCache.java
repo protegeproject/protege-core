b4e42f59d0c1ddfa1f74a04939a5f44a702171f8
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-04-28 23:23:33 +0000

    Refactor + several changes reflecting the fact that updates to the client cache are no longer represented through events.

---
+++ src/edu/stanford/smi/protege/server/update/RemoveFrameCache.java
@@ -0,0 +1,16 @@
+package edu.stanford.smi.protege.server.update;
+
+import edu.stanford.smi.protege.model.Frame;
+
+public class RemoveFrameCache extends ValueUpdate {
+
+  /**
+   * This update tells the client to remove the entire cache for the whole frame.
+   * 
+   * @param frame
+   */
+  public RemoveFrameCache(Frame frame) {
+    super(frame);
+  }
+
+}
