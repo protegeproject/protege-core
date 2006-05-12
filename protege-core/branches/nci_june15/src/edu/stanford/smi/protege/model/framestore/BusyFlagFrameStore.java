5e7d10c289a1459035ac36e91c13aa083484adfd
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-05-12 16:04:41 +0000

    A frame store to keep track of when things are busy.

---
+++ protege-core/branches/nci_june15/src/edu/stanford/smi/protege/model/framestore/BusyFlagFrameStore.java
@@ -0,0 +1,29 @@
+package edu.stanford.smi.protege.model.framestore;
+
+import java.lang.reflect.Method;
+
+public class BusyFlagFrameStore extends AbstractFrameStoreInvocationHandler {
+  public static final long QUIESCENCE_INTERVAL = 100;
+  private static boolean busy = false;
+  private static long quiescenceStarted = 0;
+  
+  
+  @Override
+  protected Object handleInvoke(Method method, Object[] args) {
+    Object result;
+    busy = true;
+    result = invoke(method, args);
+    quiescenceStarted = System.currentTimeMillis();
+    busy = false;
+    return result;
+  }
+  
+  public static boolean isBusy() {
+    if (busy) {
+      return true;
+    }
+    return System.currentTimeMillis() > quiescenceStarted + QUIESCENCE_INTERVAL;
+  }
+  
+  
+}
