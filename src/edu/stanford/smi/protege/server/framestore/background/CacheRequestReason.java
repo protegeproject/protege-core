071c56a0464198f6343f99aec60770d2248c936c
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-05-15 04:28:58 +0000

    Track the reason why a cache request was sent - this helps me prioritize.  Also good for debugging.

---
+++ src/edu/stanford/smi/protege/server/framestore/background/CacheRequestReason.java
@@ -0,0 +1,47 @@
+package edu.stanford.smi.protege.server.framestore.background;
+
+import java.util.EnumSet;
+
+public enum CacheRequestReason {
+  USER_REQUESTED_FRAME_VALUES,  USER_NAME_REQUEST, USER_CLOSURE_REQUEST, USER_SPECIFIC_FRAMES,  
+  NEW_FRAME, PRELOAD, SUBCLASS, STATE_MACHINE;
+  
+  private static int MIN_PRIORITY;
+  static {
+    int min = STATE_MACHINE.priority();
+    for (CacheRequestReason reason : CacheRequestReason.values()) {
+      if (reason.priority() < min) {
+        min = reason.priority();
+      }
+      MIN_PRIORITY = min;
+    }
+  }
+  
+  public int priority() {
+    switch (this) {
+    case NEW_FRAME:
+      return 4;
+    case STATE_MACHINE:
+      return 3;
+    case USER_REQUESTED_FRAME_VALUES:
+      return 2;
+    case USER_CLOSURE_REQUEST:
+      return 1;
+    case SUBCLASS:
+      return -1;
+    default:
+      return 0;
+    }
+  }
+  
+  public static int priority(EnumSet<CacheRequestReason> reasons) {
+    int priority = MIN_PRIORITY;
+    for (CacheRequestReason reason : reasons) {
+      if (reason.priority() > priority) {
+        priority = reason.priority();
+      }
+    }
+    return priority;
+  }
+  
+}
