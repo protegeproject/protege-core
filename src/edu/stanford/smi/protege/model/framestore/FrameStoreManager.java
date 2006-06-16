bf307693b1ac47068c10f8772144814db1c4a80b
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-06-16 02:04:15 +0000

    Kim's method

--- src/edu/stanford/smi/protege/model/framestore/FrameStoreManager.java
+++ src/edu/stanford/smi/protege/model/framestore/FrameStoreManager.java
@@ -3,6 +3,7 @@ package edu.stanford.smi.protege.model.framestore;
 //ESCA*JAVA0100
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Proxy;
 import java.net.URI;
 import java.util.Collections;
@@ -14,6 +15,9 @@ import java.util.ListIterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import edu.stanford.smi.protege.exception.ProtegeError;
+import edu.stanford.smi.protege.exception.ProtegeException;
+import edu.stanford.smi.protege.exception.ProtegeStoreException;
 import edu.stanford.smi.protege.model.Cls;
 import edu.stanford.smi.protege.model.KnowledgeBase;
 import edu.stanford.smi.protege.model.framestore.cleandispatch.CleanDispatchFrameStore;
@@ -327,6 +331,22 @@ public class FrameStoreManager {
 
     public void setPollForEvents(boolean b) {
         eventDispatchFrameStore.setPollForEvents(b);
+    }
+    
+    public void flushEvents() throws ProtegeException {
+      try {
+        eventDispatchFrameStore.getEventsAndDispatch();
+      } catch (InvocationTargetException e) {
+        Throwable cause = e.getCause();
+        if (cause instanceof ProtegeException) {
+          throw (ProtegeException) cause;
+        }
+        Log.getLogger().log(Level.WARNING, 
+                            "A listener threw an exception while processing events", e);
+        throw new ProtegeError(e);
+      } catch (InterruptedException e) {
+        throw new ProtegeStoreException(e);  // arguable - who interrupted this?
+      }
     }
 
     public void removeListener(Class c, Object o, EventListener listener) {
