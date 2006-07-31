75f9e7e3e58240507041215577a7c0a5af0551f6
vendetti <vendetti@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-07-31 21:01:05 +0000

    updated the url that the getting started button launches - it was pointing to a broken link.  this button is located on the welcome dialog.

--- protege-core/trunk/src/edu/stanford/smi/protege/util/ApplicationProperties.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/util/ApplicationProperties.java
@@ -32,7 +32,7 @@ public class ApplicationProperties {
     public static final String BROWSER = "browser.html";
     private static final String AUTOSYNCHRONIZE_PROPERTY = "trees.autosynchronize";
     private static final String PRETTY_PRINT_SLOT_WIDGET_LABELS = "labels.pretty_print";
-    
+
     public static final String LOG_FILE_PROPERTY = "java.util.logging.config.file";
     public static final String LOG_DEBUG_PROPERTY = "log.config.debug";
 
@@ -359,7 +359,7 @@ public class ApplicationProperties {
     }
 
     public static String getGettingStartedURLString() {
-        return "http://protege.stanford.edu/doc/tutorial/get_started";
+        return "http://protege.stanford.edu/doc/tutorial/get_started/table_of_content.html";
     }
 
     public static String getFAQURLString() {
