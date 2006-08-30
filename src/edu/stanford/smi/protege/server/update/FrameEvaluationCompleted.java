182f4cdd4b15316f9e6f0b716063072cc512e466
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-08-30 23:05:35 +0000

    Minor

--- src/edu/stanford/smi/protege/server/update/FrameEvaluationCompleted.java
+++ src/edu/stanford/smi/protege/server/update/FrameEvaluationCompleted.java
@@ -5,7 +5,9 @@ import edu.stanford.smi.protege.model.KnowledgeBase;
 import edu.stanford.smi.protege.model.Localizable;
 
 public class FrameEvaluationCompleted extends ValueUpdate implements Localizable {
-  
+
+  private static final long serialVersionUID = 7794961942298066290L;
+
   public FrameEvaluationCompleted(Frame frame) {
     super(frame);
   }
