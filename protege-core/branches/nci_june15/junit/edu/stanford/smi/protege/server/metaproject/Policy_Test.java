5ea2f692559b64d8263a883769f7d50028aee858
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-09-03 17:21:48 +0000

    policy test (passes)

---
+++ protege-core/branches/nci_june15/junit/edu/stanford/smi/protege/server/metaproject/Policy_Test.java
@@ -0,0 +1,33 @@
+package edu.stanford.smi.protege.server.metaproject;
+
+import edu.stanford.smi.protege.model.framestore.SimpleTestCase;
+import edu.stanford.smi.protege.server.metaproject.impl.MetaProjectImpl;
+import edu.stanford.smi.protege.server.metaproject.impl.MetaProjectInstanceImpl;
+import edu.stanford.smi.protege.server.metaproject.impl.OperationImpl;
+import edu.stanford.smi.protege.server.metaproject.impl.UserInstanceImpl;
+import edu.stanford.smi.protege.util.URIUtilities;
+
+public class Policy_Test extends SimpleTestCase {
+  
+  public static final String METAPROJECT = "junit/pprj/policy/metaproject.pprj";
+  
+  public static final MetaProjectInstance PROJECT1 = new MetaProjectInstanceImpl("Newspaper");
+  
+  public static final UserInstance        NATASHA  = new UserInstanceImpl("Natasha Noy");
+  public static final UserInstance        BOB      = new UserInstanceImpl("Bob");
+  public static final UserInstance        PAUL     = new UserInstanceImpl("Paul");
+  
+  public static final Operation           RESTART  = new OperationImpl("RestartServer");   
+  
+  public void testPolicy01() {
+    MetaProject mp = new MetaProjectImpl(URIUtilities.createURI(METAPROJECT));
+    
+    
+    Policy p = mp.getPolicy(PROJECT1);
+
+    assertTrue( p.isOperationAuthorized(NATASHA, OperationImpl.EDIT));
+    assertFalse(p.isOperationAuthorized(BOB,     OperationImpl.EDIT));
+    assertFalse(p.isOperationAuthorized(PAUL,    OperationImpl.EDIT));
+    assertTrue( p.isOperationAuthorized(PAUL,    RESTART));
+  }
+}
