e8cadac92964d1831e915ee6e7486e21e17e75d5
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-09-07 18:52:52 +0000

    Minor fixes so that it runs with os x.

--- protege-core/branches/nci_june15/bin/run_protege_server.sh
+++ protege-core/branches/nci_june15/bin/run_protege_server.sh
@@ -3,12 +3,19 @@
 # Change to the script' working directory, should be the Protege root directory 
 cd $(dirname $0)
 
-# Attempt to use the bundled VM if none specified
-if [ "$JAVA_HOME" == "" ]; then
-	JAVA_HOME=.
-fi
 
-JAVA_PATH=$JAVA_HOME/jre/bin
+
+if [  -x /usr/bin/uname  -a  `/usr/bin/uname` == "Darwin" ] 
+then
+  JAVA_PATH=/usr/bin
+else 
+  # Attempt to use the bundled VM if none specified
+  if [ "$JAVA_HOME" == "" ]; then
+	JAVA_HOME=.
+  fi
+
+  JAVA_PATH=$JAVA_HOME/jre/bin
+fi
 
 # Check if the Java VM can be found 
 if [ ! -e $JAVA_PATH/java ]; then
@@ -28,6 +35,14 @@ CODEBASE=-Djava.rmi.server.codebase=$CODEBASE_URL
 HOSTNAME_PARAM=-Djava.rmi.server.hostname=$HOSTNAME
 OPTIONS="$SAVE_INTERVAL $MAX_MEMORY $CODEBASE $HOSTNAME_PARAM"
 METAPROJECT=examples/server/metaproject.pprj
+TX="-Dtransaction.level=READ_COMMITTED"
+
+#
+# Instrumentation debug, delay simulation,  etc
+#
+#DELAY="-Dserver.delay=80"
+#DEBUG_OPT="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
+
 
 $JAVA_PATH/rmiregistry &
-$JAVA_PATH/java -cp $CLASSPATH $OPTIONS $MAINCLASS $METAPROJECT
+$JAVA_PATH/java -cp $CLASSPATH $TX $OPTIONS $MAINCLASS $METAPROJECT
