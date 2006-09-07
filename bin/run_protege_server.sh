41334c53e1eb32aa0512a6f60d0068e06df53c4c
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-09-07 19:14:14 +0000

    fixing run_protege_server to run on both linux and os x.

--- bin/run_protege_server.sh
+++ bin/run_protege_server.sh
@@ -3,9 +3,7 @@
 # Change to the script' working directory, should be the Protege root directory 
 cd $(dirname $0)
 
-
-
-if [  -x /usr/bin/uname  -a  `/usr/bin/uname` == "Darwin" ] 
+if [  -x /usr/bin/uname  -a  "x`/usr/bin/uname`" == "Darwin" ] 
 then
   JAVA_PATH=/usr/bin
 else 
