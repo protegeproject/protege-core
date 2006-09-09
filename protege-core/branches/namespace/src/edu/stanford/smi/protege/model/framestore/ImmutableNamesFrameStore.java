7a3a9fcf2cd39d5e29ba4c9d49b64ab20da3a6f1
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-09-09 20:12:08 +0000

    Cleaned up a little.  There is also a fix for making sure that the name of a frame sticks but this is commented out because I think this is done by the InMemoryFrameStore.

--- protege-core/branches/namespace/src/edu/stanford/smi/protege/model/framestore/ImmutableNamesFrameStore.java
+++ protege-core/branches/namespace/src/edu/stanford/smi/protege/model/framestore/ImmutableNamesFrameStore.java
@@ -2,30 +2,44 @@ package edu.stanford.smi.protege.model.framestore;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.EventObject;
 import java.util.List;
-import java.util.Set;
 
 import edu.stanford.smi.protege.model.Cls;
+import edu.stanford.smi.protege.model.DefaultSlot;
 import edu.stanford.smi.protege.model.Facet;
 import edu.stanford.smi.protege.model.Frame;
 import edu.stanford.smi.protege.model.FrameID;
-import edu.stanford.smi.protege.model.Instance;
 import edu.stanford.smi.protege.model.KnowledgeBase;
 import edu.stanford.smi.protege.model.Model;
 import edu.stanford.smi.protege.model.SimpleInstance;
 import edu.stanford.smi.protege.model.Slot;
-import edu.stanford.smi.protege.model.query.Query;
 
 public class ImmutableNamesFrameStore extends FrameStoreAdapter {
   private KnowledgeBase kb;
+  private Slot nameSlot;
   
   public ImmutableNamesFrameStore(KnowledgeBase kb) {
     this.kb = kb;
+    nameSlot = new DefaultSlot(kb,Model.SlotID.NAME); //using system frames here leads to circularity
   }
 
-  public String getFrameName(Frame frame) {
-    return frame.getFrameID().getName();
+  private boolean isNameSlot(Slot slot) {
+    return slot.equals(nameSlot);
+  }
+  
+  public FrameID adjustFrameId(FrameID id) {
+    if (id == null || id.getName() == null) {
+      return new FrameID(generateUniqueName("Class"));
+    } else {
+      checkUniqueness(id.getName());
+      return id;
+    }
+  }
+  
+  private void checkNotNameSlot(Slot slot) {
+    if (isNameSlot(slot)) {
+      throw new IllegalArgumentException("Should not be modifying name slot values");
+    }
   }
   
   protected void checkUniqueness(String name) {
@@ -51,66 +65,63 @@ public class ImmutableNamesFrameStore extends FrameStoreAdapter {
     }
     return uniqueName;
   }
-
-  private boolean isNameSlot(Slot slot) {
-    return slot.getFrameID().equals(Model.SlotID.NAME);
+  
+  
+  private void insertName(Frame frame) {
+    /*
+    List<String> names = new ArrayList<String>();
+    names.add(frame.getFrameID().getName());
+    getDelegate().setDirectOwnSlotValues(frame, nameSlot, names);
+    */
   }
   
-  private void checkNotNameSlot(Slot slot) {
-    if (isNameSlot(slot)) {
-      throw new IllegalArgumentException("Should not be modifying name slot values");
-    }
-  }
+
   
   /*
    * ---------------------------------------------------------------------------
    * FrameStore calls.
    */
+  
+  public String getFrameName(Frame frame) {
+    return frame.getFrameID().getName();
+  }
 
   public Cls createCls(FrameID id, 
                        Collection directTypes, 
                        Collection directSuperclasses, 
                        boolean loadDefaultValues) {
-    if (id == null || id.getName() == null) {
-      id = new FrameID(generateUniqueName("Class"));
-    } else {
-      checkUniqueness(id.getName());
-    }
-    return getDelegate().createCls(id, directTypes, directSuperclasses, loadDefaultValues);
+    id = adjustFrameId(id);
+    Cls cls = getDelegate().createCls(id, directTypes, directSuperclasses, loadDefaultValues);
+    insertName(cls);
+    return cls;
   }
 
   public Slot createSlot(FrameID id, Collection directTypes, Collection directSuperslots, boolean loadDefaultValues) {
-    if (id == null || id.getName() == null) {
-      id = new FrameID(generateUniqueName("Slot"));
-    } else {
-      checkUniqueness(id.getName());
-    }
-    return getDelegate().createSlot(id, directTypes, directSuperslots, loadDefaultValues);
+    id = adjustFrameId(id);
+    Slot slot = getDelegate().createSlot(id, directTypes, directSuperslots, loadDefaultValues);
+    insertName(slot);
+    return slot;
   }
 
   public Facet createFacet(FrameID id, Collection directTypes, boolean loadDefaultValues) {
-    if (id == null || id.getName() == null) {
-      id = new FrameID(generateUniqueName("Facet"));
-    } else {
-      checkUniqueness(id.getName());
-    }
-    return getDelegate().createFacet(id, directTypes, loadDefaultValues);
+    id = adjustFrameId(id);
+    Facet facet =  getDelegate().createFacet(id, directTypes, loadDefaultValues);
+    insertName(facet);
+    return facet;
   }
 
   public SimpleInstance createSimpleInstance(FrameID id,
                                              Collection directTypes,
                                              boolean loadDefaultValues) {
-    if (id == null || id.getName() == null) {
-      id = new FrameID(generateUniqueName("Instance"));
-    } else {
-      checkUniqueness(id.getName());
-    }
-    return getDelegate().createSimpleInstance(id, directTypes, loadDefaultValues);
+    id = adjustFrameId(id);
+    SimpleInstance instance = getDelegate().createSimpleInstance(id, directTypes, loadDefaultValues);
+    insertName(instance);
+    return instance;
   }
 
   @SuppressWarnings("unchecked")
   public Collection getOwnSlotValues(Frame frame, Slot slot) {
-    if (slot.getFrameID().equals(Model.SlotID.NAME)) {
+    if (isNameSlot(slot)) {
       Collection results = new ArrayList();
       results.add(frame.getFrameID().getName());
       return results;
@@ -120,7 +131,7 @@ public class ImmutableNamesFrameStore extends FrameStoreAdapter {
 
   @SuppressWarnings("unchecked")
   public List getDirectOwnSlotValues(Frame frame, Slot slot) {
-    if (slot.getFrameID().equals(Model.SlotID.NAME)) {
+    if (isNameSlot(slot)) {
       List results = new ArrayList();
       results.add(frame.getFrameID().getName());
       return results;
