03911837207e001072d5af8944fe1144eee6e4cf
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-09-10 15:54:48 +0000

    Updates to the metaproject interface for the new metaproject format.

--- src/edu/stanford/smi/protege/server/metaproject/impl/PolicyImpl.java
+++ src/edu/stanford/smi/protege/server/metaproject/impl/PolicyImpl.java
@@ -1,108 +1,60 @@
 package edu.stanford.smi.protege.server.metaproject.impl;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
-import edu.stanford.smi.protege.model.Instance;
-import edu.stanford.smi.protege.server.metaproject.Authorization;
+import edu.stanford.smi.protege.server.metaproject.GroupInstance;
 import edu.stanford.smi.protege.server.metaproject.MetaProjectInstance;
 import edu.stanford.smi.protege.server.metaproject.Operation;
 import edu.stanford.smi.protege.server.metaproject.Policy;
 import edu.stanford.smi.protege.server.metaproject.UserInstance;
-import edu.stanford.smi.protege.server.metaproject.impl.MetaProjectImpl.ClsEnum;
-import edu.stanford.smi.protege.util.Log;
 
 public class PolicyImpl implements Policy {
   MetaProjectImpl mp;
-  private List<Authorization> authorizations;
-  private List<Authorization> authorizationsReverseOrder;
-  private Set<Operation> knownOperations;
   
   public PolicyImpl(MetaProjectImpl mp) {
     this.mp = mp;
   }
   
-  public List<Authorization> getAuthorizations() {
-    if (authorizations == null) {
-      authorizations = new  ArrayList<Authorization>();
-      for (Object auth : ClsEnum.Authorization.getCls(mp).getInstances()) {
-        if (auth instanceof Instance) {
-          authorizations.add(new AuthorizationImpl(mp, (Instance) auth));
-        } else {
-          Log.getLogger().warning("Object " + auth + " in " + ClsEnum.Authorization 
-                                  + " class is not an instnace.");
-        }
+  private UserInstance normalizeUser(UserInstance user) {
+    for (UserInstance normalizedUser : mp.getUserInstances()) {
+      if (user.getName().equals(normalizedUser.getName())) {
+        return normalizedUser;
       }
-      Collections.sort(authorizations, new Comparator<Authorization>() {
-        public int compare(Authorization a1, Authorization a2) {
-          if (a1.getPriority() > a2.getPriority()) {
-            return 1;
-          }
-          else if (a1.getPriority()  < a2.getPriority()) {
-            return -1;
-          } else {
-            int allow1 = a1.isAllowed() ? 1 : 0;
-            int allow2 = a2.isAllowed() ? 1 : 0;
-            if (allow1 < allow2) {
-              return 1;
-            }
-            else if (allow2 > allow1) {
-              return -1;
-            }
-            else {
-              return 0;
-            }
-          }
-        }      
-      });
     }
-    return authorizations;
+    throw new IllegalArgumentException("Unknown user " + user);
   }
 
-  public boolean isOperationAuthorized(UserInstance user, 
-                                       Operation op, 
-                                       MetaProjectInstance project) {
+  /*
+   * The project is ignored in this implementation.
+   */
+  public boolean isOperationAuthorized(UserInstance user, Operation op, MetaProjectInstance project) {
+    user = normalizeUser(user);
     if (!getKnownOperations().contains(op)) {
       return true;
     }
-    if (authorizationsReverseOrder == null) {
-      authorizationsReverseOrder = new ArrayList<Authorization>(getAuthorizations());
-      Collections.reverse(authorizationsReverseOrder);
-    }
-    for (Authorization auth : authorizationsReverseOrder) {
-      if (auth.getActors().contains(user)  && auth.getOperations().contains(op) && auth.getProjects().contains(project)) {
-        return auth.isAllowed();
+    for (GroupInstance group  : user.getGroups()) {
+      if (group.getAllowedOperations().contains(op)) {
+        return true;
       }
     }
     return false;
   }
 
+  /*
+   * The project is ignored in this implementation.
+   */
   public Set<Operation> getAllowedOperations(UserInstance user, MetaProjectInstance project) {
-    Set<Operation> allowedOps = new HashSet<Operation>();
-    for (Authorization auth  : getAuthorizations()) {
-      if (auth.getActors().contains(user) && auth.getProjects().contains(project)) {
-        if (auth.isAllowed()) {
-          allowedOps.addAll(auth.getOperations());
-        } else  {
-          allowedOps.removeAll(auth.getOperations());
-        }
-      }
+    user = normalizeUser(user);
+    Set<Operation> allowed = new HashSet<Operation>();
+    for (GroupInstance group  : user.getGroups()) {
+      allowed.addAll(group.getAllowedOperations());
     }
-    return allowedOps;
+    return allowed;
   }
-  
+
   public Set<Operation> getKnownOperations() {
-    if (knownOperations == null) {
-      knownOperations = new HashSet<Operation>();
-      for (Instance op : ClsEnum.Operation.getCls(mp).getInstances()) {
-        knownOperations.add(new OperationImpl(mp, op));
-      }
-    }
-    return knownOperations;
+    return mp.getOperations();
   }
 
 }
--- src/edu/stanford/smi/protege/server/metaproject/impl/UserInstanceImpl.java
+++ src/edu/stanford/smi/protege/server/metaproject/impl/UserInstanceImpl.java
@@ -1,17 +1,22 @@
 package edu.stanford.smi.protege.server.metaproject.impl;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import edu.stanford.smi.protege.model.Instance;
+import edu.stanford.smi.protege.server.metaproject.GroupInstance;
 import edu.stanford.smi.protege.server.metaproject.UserInstance;
 import edu.stanford.smi.protege.server.metaproject.impl.MetaProjectImpl.ClsEnum;
 import edu.stanford.smi.protege.server.metaproject.impl.MetaProjectImpl.SlotEnum;
+import edu.stanford.smi.protege.util.Log;
 
 public class UserInstanceImpl implements UserInstance, Serializable {
   private static final long serialVersionUID = -4416984896523630762L;
   
   String name;
   String password;
+  Set<GroupInstance> groups;
   
   protected UserInstanceImpl(MetaProjectImpl mp, Instance ui) {
     if (!ui.getDirectTypes().contains(ClsEnum.User.getCls(mp))) {
@@ -19,6 +24,15 @@ public class UserInstanceImpl implements UserInstance, Serializable {
     }
     name = (String) ui.getOwnSlotValue(SlotEnum.name.getSlot(mp));
     password = (String) ui.getOwnSlotValue(SlotEnum.password.getSlot(mp));
+    
+    groups = new HashSet<GroupInstance>();
+    for (Object instance : ui.getOwnSlotValues(SlotEnum.groups.getSlot(mp))) {
+      try {
+        groups.add(new GroupInstanceImpl(mp, (Instance) instance));
+      } catch (ClassCastException cce) {
+        Log.getLogger().warning("Metaproject invalid because the allowed Operations of a group should be instances of operations");
+      }
+    }
   }
   
   public UserInstanceImpl(String name) {
@@ -31,6 +45,10 @@ public class UserInstanceImpl implements UserInstance, Serializable {
 
   public String getPassword() {
     return password;
+  }
+  
+  public Set<GroupInstance> getGroups() {
+    return groups;
   }
   
   public boolean equals(Object o) { 
