c7e2c6ff3e582421681e7b108978cb12e7670b7e
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-09-10 15:54:29 +0000

    Updates to the metaproject interface for the new metaproject format.

--- protege-core/branches/nci_june15/src/edu/stanford/smi/protege/server/metaproject/GroupInstance.java
+++ protege-core/branches/nci_june15/src/edu/stanford/smi/protege/server/metaproject/GroupInstance.java
@@ -2,10 +2,11 @@ package edu.stanford.smi.protege.server.metaproject;
 
 import java.util.Set;
 
+
 public interface GroupInstance {
   
   String getName();
   
-  Set<UserInstance> getMembers();
+  Set<Operation> getAllowedOperations();
 
 }
