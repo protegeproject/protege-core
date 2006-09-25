c91160d25eb3feb990d4bda4d2703e6d2164e6ee
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-09-25 20:42:40 +0000

    added method for getting all application properities

--- protege-core/trunk/src/edu/stanford/smi/protege/util/ApplicationProperties.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/util/ApplicationProperties.java
@@ -235,6 +235,10 @@ public class ApplicationProperties {
         return rectangle;
     }
 
+    public static Properties getApplicationProperties() {
+    	return PROPERTIES;
+    }
+    
     public static String getApplicationOrSystemProperty(String name) {
         return getApplicationOrSystemProperty(name, null);
     }
