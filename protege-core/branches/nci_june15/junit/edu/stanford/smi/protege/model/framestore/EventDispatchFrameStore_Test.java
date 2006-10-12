d01a895e88a5874190477a585cf1617999d0ee5d
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2006-10-12 18:58:26 +0000

    adjust the test to account for changes to EventDispatchFrameStore.java

--- protege-core/branches/nci_june15/junit/edu/stanford/smi/protege/model/framestore/EventDispatchFrameStore_Test.java
+++ protege-core/branches/nci_june15/junit/edu/stanford/smi/protege/model/framestore/EventDispatchFrameStore_Test.java
@@ -5,7 +5,7 @@ import edu.stanford.smi.protege.model.*;
 public class EventDispatchFrameStore_Test extends FrameStore_Test {
 
     protected FrameStore     createFrameStore(DefaultKnowledgeBase kb){
-        FrameStore fs = new EventDispatchFrameStore();
+        FrameStore fs = new EventDispatchFrameStore(kb);
         fs.setDelegate(new InMemoryFrameStore(kb));
         return fs;
     }
