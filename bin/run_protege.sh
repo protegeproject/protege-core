5477695a6c68aaa510894e4667991906efa9c04f
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2006-10-28 00:57:27 +0000

    modified to play well with os x

--- bin/run_protege.sh
+++ bin/run_protege.sh
@@ -3,12 +3,17 @@
 # Change to the script' working directory, should be the Protege root directory 
 cd $(dirname $0)
 
-# Attempt to use the bundled VM if none specified
-if [ "$JAVA_HOME" == "" ]; then
+if [  -x /usr/bin/uname  -a "x`/usr/bin/uname`" == "xDarwin" ] 
+then
+  JAVA_PATH=/usr/bin
+else 
+  # Attempt to use the bundled VM if none specified
+  if [ "$JAVA_HOME" == "" ]; then
 	JAVA_HOME=.
-fi
+  fi
 
-JAVA_PATH=$JAVA_HOME/jre/bin
+  JAVA_PATH=$JAVA_HOME/jre/bin
+fi
 
 # Check if the Java VM can be found 
 if [ ! -e $JAVA_PATH/java ]; then
@@ -20,6 +25,9 @@ JARS=protege.jar:driver.jar:driver1.jar:looks.jar:unicode_panel.jar
 MAIN_CLASS=edu.stanford.smi.protege.Application
 MAXIMUM_MEMORY=-Xmx100M
 OPTIONS=$MAXIMUM_MEMORY
+
+#Possible instrumentation options - debug, etc.
+#DEBUG_OPT="-Xdebug -Xrunjdwp:transport=dt_socket,address=8100,server=y,suspend=n"
 
 # Run Protege
 $JAVA_PATH/java $OPTIONS -cp $JARS $MAIN_CLASS $1
