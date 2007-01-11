aae95ffcc160cc47758c1a94ff1477c247fd723e
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2007-01-11 01:41:16 +0000

    This guy is no longer a singleton but an equals/hashCode method has been added. The only difference between different object of this class is whether the represent ssl sockets (which is set by policy).

--- src/edu/stanford/smi/protege/server/ServerRmiSocketFactory.java
+++ src/edu/stanford/smi/protege/server/ServerRmiSocketFactory.java
@@ -1,10 +1,11 @@
 package edu.stanford.smi.protege.server;
 
-import java.io.*;
-import java.net.*;
-import java.rmi.server.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.rmi.server.RMIServerSocketFactory;
 
-import edu.stanford.smi.protege.util.*;
+import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protege.util.StringUtilities;
 
 /* This code is based on an idea from here:
  * http://www.javacoding.net/articles/technical/rmi-firewall.html
@@ -14,25 +15,20 @@ import edu.stanford.smi.protege.util.*;
  */
 
 public class ServerRmiSocketFactory implements RMIServerSocketFactory {
-    
-    private static ServerRmiSocketFactory instance;
-    private int fixedPort;
+	
+    private boolean use_ssl;
 
-    private ServerRmiSocketFactory() {
-        fixedPort = getServerPort();
-        if (fixedPort != 0) {
-            Log.getLogger().config("fixed port=" + fixedPort);
+    public ServerRmiSocketFactory(SSLSettings.Context context) {
+    	use_ssl = SSLSettings.useSSL(context);
+        if (getServerPort(context) != 0) {
+            Log.getLogger().config("fixed port=" + getServerPort(context));
         }
     }
     
-    public static ServerRmiSocketFactory getInstance() {
-        if (instance == null) {
-            instance = new ServerRmiSocketFactory();
-        }
-        return instance;
-    }
-    
-    public static int getServerPort() {
+    public static int getServerPort(SSLSettings.Context context) {
+    	if (SSLSettings.useSSL(context)) {
+    		return Integer.getInteger(ClientRmiSocketFactory.SERVER_SSL_PORT, 0).intValue();
+    	}
         return Integer.getInteger(ClientRmiSocketFactory.SERVER_PORT, 0).intValue();
     }
 
@@ -40,22 +36,30 @@ public class ServerRmiSocketFactory implements RMIServerSocketFactory {
      * This method gets passed a 0 to indicate "allocate any port".  If the user has specfied a
      * port then we use it.
      */
-    public ServerSocket createServerSocket(int requestedPort) throws IOException {
-        int port = requestedPort == 0 ? fixedPort : requestedPort;
+    public ServerSocket createServerSocket(int port) throws IOException {
         ServerSocket socket;
-        if (SSLSettings.useSSL()) {
+        if (use_ssl) {
             socket = new SSLSettings().createSSLServerSocket(port);
         }
         else {
             socket = new ServerSocket(port);
-        }
-        if (fixedPort != 0) {
-            Log.getLogger().config("local port: " + socket.getLocalPort());
         }
         return socket;
     }
     
     public String toString() {
         return StringUtilities.getClassName(this);
+    }
+    
+    public boolean equals(Object o) {
+    	if (!(o instanceof ServerRmiSocketFactory)) {
+    		return false;
+    	}
+    	ServerRmiSocketFactory other = (ServerRmiSocketFactory) o;
+    	return use_ssl == other.use_ssl;
+    }
+    
+    public int hashCode() {
+    	return use_ssl ? 1 : 0;
     }
 }
