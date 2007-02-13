4502459d08e859010ff98de33e0bbef49b20c146
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-02-13 01:38:00 +0000

    merged with head: from rev. 4873 to 5067

--- protege-core/branches/collab/src/edu/stanford/smi/protege/util/ComponentUtilities.java
+++ protege-core/branches/collab/src/edu/stanford/smi/protege/util/ComponentUtilities.java
@@ -211,10 +211,10 @@ public class ComponentUtilities {
         for (int i = 0; i < nChildren; ++i) {
             childNode = (LazyTreeNode) node.getChildAt(i);
             if (equals(childNode.getUserObject(), userObject)) {
-                break;
+                return childNode;
             }
         }
-        return childNode;
+        return null;
     }
 
     public static boolean equals(Object o1, Object o2) {
@@ -397,7 +397,7 @@ public class ComponentUtilities {
             Object userObject = i.next();
             node = getChildNode(node, userObject);
             if (node == null) {
-                Log.getLogger().warning("Child node not found: " + userObject);
+                //Log.getLogger().warning("Child node not found: " + userObject);
                 return null;
             }
             nodePath.add(node);
