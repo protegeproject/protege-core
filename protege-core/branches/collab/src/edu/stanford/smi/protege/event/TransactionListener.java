3c39f8a4e76cd5362110e4c5c3da58ba4d49ec2a
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-02-22 19:15:16 +0000

    We don't need the collab branch. It was the same as the trunk

--- protege-core/branches/collab/src/edu/stanford/smi/protege/event/TransactionListener.java
+++
@@ -1,9 +0,0 @@
-package edu.stanford.smi.protege.event;
-
-import java.util.*;
-
-public interface TransactionListener extends EventListener{
-    void transactionBegin(TransactionEvent event);
-    void transactionEnded(TransactionEvent event);
-
-}
--- protege-core/branches/collab/src/edu/stanford/smi/protege/util/Traverser.java
+++
@@ -1,9 +0,0 @@
-package edu.stanford.smi.protege.util;
-
-/**
- * Traverse from one object to the next
- * @author Ray Fergerson <fergerson@smi.stanford.edu>
- */
-public interface Traverser {
-    Object get(Object o);
-}
