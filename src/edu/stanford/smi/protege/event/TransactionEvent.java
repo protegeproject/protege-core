f9ff6fca69b306257796211f6dfd069d7a39e97e
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2007-03-09 00:09:07 +0000

    reverting back from previous commit

--- src/edu/stanford/smi/protege/event/TransactionEvent.java
+++ src/edu/stanford/smi/protege/event/TransactionEvent.java
@@ -1,15 +1,15 @@
 package edu.stanford.smi.protege.event;
 
-import edu.stanford.smi.protege.model.*;
-import edu.stanford.smi.protege.util.*;
+import edu.stanford.smi.protege.model.KnowledgeBase;
+import edu.stanford.smi.protege.util.AbstractEvent;
 
 public class TransactionEvent extends AbstractEvent {
     private static final int BASE = 700;
     public static final int TRANSACTION_BEGIN = BASE + 1;
     public static final int TRANSACTION_END = BASE + 2;
     
-    public TransactionEvent(KnowledgeBase kb, int type, String name, Frame applyTo) {
-        super(kb, type, name, applyTo);
+    public TransactionEvent(KnowledgeBase kb, int type, String name) {
+        super(kb, type, name);
     }
     
     public KnowledgeBase getKnowledgeBase() {
@@ -19,8 +19,5 @@ public class TransactionEvent extends AbstractEvent {
     public String getBeginString() {
         return (String) getArgument();
     }
-    
-    public Frame getApplyTo() {
-        return (Frame) getArgument2();
-    }
+
 }
