0337b95617d082a3ae74f3947c9d3c2134c292ce
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-04-18 21:35:46 +0000

    beautifying and fix

--- protege-core/branches/merged/bin/run_protege_server.sh
+++ protege-core/branches/merged/bin/run_protege_server.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
 
+# ------------------- Where is Java? ------------------- 
+
 # Change to the script' working directory, should be the Protege root directory 
 cd $(dirname $0)
 
-if [  -x /usr/bin/uname  -a  "x`/usr/bin/uname`" == "xDarwin" ] 
+DARWIN="false"
+
+if [  -x /usr/bin/uname ]
+then
+  if [ "x`/usr/bin/uname`" = "xDarwin" ] 
+  then
+    DARWIN="true"
+  fi
+fi
+
+if [ ${DARWIN} = "true" ]
 then
   JAVA_PATH=/usr/bin
 else 
   # Attempt to use the bundled VM if none specified
-  if [ "$JAVA_HOME" == "" ]; then
+  if [ "$JAVA_HOME" = "" ]; then
 	JAVA_HOME=.
   fi
 
@@ -20,18 +32,38 @@ if [ ! -e $JAVA_PATH/java ]; then
 	echo Java VM could not be found. Please check your JAVA_HOME environment variable.
 	exit 1
 fi
+# ------------------- Where is Java? ------------------- 
+
 
 CLASSPATH=protege.jar:looks.jar:unicode_panel.jar:driver.jar:driver0.jar:driver1.jar:driver2.jar
 MAINCLASS=edu.stanford.smi.protege.server.Server
 
-# If you want automatic saving of the project, setup the number of seconds in SAVE_INTERVAL_VALUE
-# SAVE_INTERVAL=-DsaveIntervalSec=120
 
+# ------------------- JVM Options ------------------- 
 MAX_MEMORY=-Xmx100M
 CODEBASE_URL=file:$PWD/protege.jar
 CODEBASE=-Djava.rmi.server.codebase=$CODEBASE_URL
 HOSTNAME_PARAM=-Djava.rmi.server.hostname=$HOSTNAME
-OPTIONS="$SAVE_INTERVAL $MAX_MEMORY $CODEBASE $HOSTNAME_PARAM"
+TX="-Dtransaction.level=READ_COMMITTED"
+OPTIONS="$MAX_MEMORY $CODEBASE $HOSTNAME_PARAM ${TX}"
+
+#
+# Instrumentation debug, delay simulation,  etc
+#
+#DELAY="-Dserver.delay=80"
+#PORTOPTS="-Dprotege.rmi.server.port=5200 -Dprotege.rmi.registry.port=5100"
+#DEBUG_OPT="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
+
+
+OPTIONS="${OPTIONS} ${DELAY} ${PORTOPTS} ${DEBUG_OPT}"
+# ------------------- JVM Options ------------------- 
+
+# ------------------- Cmd Options -------------------
+# If you want automatic saving of the project, 
+# setup the number of seconds in SAVE_INTERVAL_VALUE
+# SAVE_INTERVAL=-saveIntervalSec=120
+# ------------------- Cmd Options -------------------
+
 METAPROJECT=examples/server/metaproject.pprj
 TX="-Dtransaction.level=READ_COMMITTED"
 
@@ -41,6 +73,5 @@ TX="-Dtransaction.level=READ_COMMITTED"
 #DELAY="-Dserver.delay=80"
 #DEBUG_OPT="-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
 
-
 $JAVA_PATH/rmiregistry &
-$JAVA_PATH/java -cp $CLASSPATH $TX $OPTIONS $MAINCLASS $METAPROJECT
+$JAVA_PATH/java -cp $CLASSPATH $TX $OPTIONS $MAINCLASS $SAVE_INTERVAL $METAPROJECT
