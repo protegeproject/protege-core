6a4450546c381ed5e7d350842eec5de373b66d4c
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-04-18 21:24:15 +0000

    imports

--- protege-core/branches/merged/src/edu/stanford/smi/protege/util/FileUtilities.java
+++ protege-core/branches/merged/src/edu/stanford/smi/protege/util/FileUtilities.java
@@ -1,10 +1,27 @@
 package edu.stanford.smi.protege.util;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.Properties;
 
-import edu.stanford.smi.protege.model.*;
+import edu.stanford.smi.protege.model.Project;
 
 /**
  * A utility class for working with files.
