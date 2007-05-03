89f4884b5a19158d26e0b34373d1a87419eb37b7
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-05-03 00:08:13 +0000

    chose a similar fix

--- protege-core/branches/merged/junit/edu/stanford/smi/protege/server/framestore/RemoteClientFrameStore_Test.java
+++ protege-core/branches/merged/junit/edu/stanford/smi/protege/server/framestore/RemoteClientFrameStore_Test.java
@@ -1,9 +1,12 @@
 package edu.stanford.smi.protege.server.framestore;
 
+import java.util.logging.Level;
+
 import edu.stanford.smi.protege.model.DefaultKnowledgeBase;
 import edu.stanford.smi.protege.model.framestore.FrameStore;
 import edu.stanford.smi.protege.model.framestore.FrameStore_Test;
 import edu.stanford.smi.protege.server.Server_Test;
+import edu.stanford.smi.protege.util.Log;
 
 public class RemoteClientFrameStore_Test extends FrameStore_Test {
 
@@ -16,7 +19,8 @@ public class RemoteClientFrameStore_Test extends FrameStore_Test {
         try {
           Server_Test.startServer();
         } catch (Exception e) {
-          fail(e.getMessage());
+            Log.getLogger().log(Level.WARNING, "Exception setting up server - tests will fail", e);
+            return null;
         }
         // Tests are a litle more deterministic if the disabled flag is turned on
         // Is there a way to make this happen without using a static? (problem = junit and fork)
