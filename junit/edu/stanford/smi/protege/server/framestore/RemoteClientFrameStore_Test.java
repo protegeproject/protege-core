a53f344d3722380d0f1e647407acf1a1599766b0
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2007-05-03 01:10:46 +0000

    Moving to the merged nci/head

---
+++ junit/edu/stanford/smi/protege/server/framestore/RemoteClientFrameStore_Test.java
@@ -0,0 +1,34 @@
+package edu.stanford.smi.protege.server.framestore;
+
+import java.util.logging.Level;
+
+import edu.stanford.smi.protege.model.DefaultKnowledgeBase;
+import edu.stanford.smi.protege.model.framestore.FrameStore;
+import edu.stanford.smi.protege.model.framestore.FrameStore_Test;
+import edu.stanford.smi.protege.server.Server_Test;
+import edu.stanford.smi.protege.util.Log;
+
+public class RemoteClientFrameStore_Test extends FrameStore_Test {
+
+    protected FrameStore createFrameStore(DefaultKnowledgeBase kb) {
+        String user = "Ray Fergerson";
+        String password = "claudia";
+        String host = "localhost";
+        String projectName = "Newspaper";
+        
+        try {
+          Server_Test.startServer();
+        } catch (Exception e) {
+            Log.getLogger().log(Level.WARNING, "Exception setting up server - tests will fail", e);
+            return null;
+        }
+        // Tests are a litle more deterministic if the disabled flag is turned on
+        // Is there a way to make this happen without using a static? (problem = junit and fork)
+        // FrameCalculator.setDisabled(true); 
+        return new RemoteClientFrameStore(host, user, password, projectName, kb, false);
+    }
+    
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
---
+++ src/edu/stanford/smi/protege/server/metaproject/MetaProjectInstance.java
@@ -0,0 +1,18 @@
+package edu.stanford.smi.protege.server.metaproject;
+
+import java.util.Set;
+
+import edu.stanford.smi.protege.model.Instance;
+
+public interface MetaProjectInstance {
+  
+  String getName();
+  
+  String getLocation();
+  
+  Set<GroupAndOperation> getAllowedGroupOperations();
+  
+  Instance getProtegeInstance();
+  
+  MetaProject getMetaProject();
+}
