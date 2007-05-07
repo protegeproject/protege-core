53383728c2fbdb5b6d717f3ec8e6912614e86212
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-05-07 02:39:55 +0000

    Added the clone session method and made not cloning session for the same project an error.

--- protege-core/trunk/src/edu/stanford/smi/protege/server/RemoteServer.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/server/RemoteServer.java
@@ -4,8 +4,13 @@ import java.rmi.*;
 import java.util.*;
 
 public interface RemoteServer extends Remote {
+    
     void reinitialize() throws RemoteException;
+    
     RemoteSession openSession(String username, String userMachine, String password) throws RemoteException;
+    
+    RemoteSession cloneSession(RemoteSession session) throws RemoteException;
+    
     void closeSession(RemoteSession session) throws RemoteException;
 
     Collection getAvailableProjectNames(RemoteSession session) throws RemoteException;
