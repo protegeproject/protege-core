e5d9e6a5153b7647c58d2d75540c72d0f849950f
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-11-01 21:51:03 +0000

    added methods to create users and save the metaproject

--- protege-core/branches/nci_june15/src/edu/stanford/smi/protege/server/metaproject/MetaProject.java
+++ protege-core/branches/nci_june15/src/edu/stanford/smi/protege/server/metaproject/MetaProject.java
@@ -1,5 +1,6 @@
 package edu.stanford.smi.protege.server.metaproject;
 
+import java.util.Collection;
 import java.util.Set;
 
 import edu.stanford.smi.protege.model.Cls;
@@ -30,6 +31,12 @@ public interface MetaProject {
   Policy getPolicy();
   
   KnowledgeBase getKnowledgeBase();
+  
+  MetaProjectInstance createMetaProjectInstance(String name);
+  
+  UserInstance createUserInstance(String name, String password);
+  
+  boolean save(Collection errors);
   
 }
 
