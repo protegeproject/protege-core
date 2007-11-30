055d01cfad15273bbf34b395b7609e980d1d277f
Tania Tudorache <tudorache@users.noreply.github.com>
2007-11-30 00:39:01 +0000

    refactored the metaproject API

--- junit/edu/stanford/smi/protege/server/metaproject/Policy_Test.java
+++ junit/edu/stanford/smi/protege/server/metaproject/Policy_Test.java
@@ -1,34 +1,33 @@
 package edu.stanford.smi.protege.server.metaproject;
 
-import java.util.HashSet;
 import java.util.Set;
 
 import edu.stanford.smi.protege.model.framestore.SimpleTestCase;
 import edu.stanford.smi.protege.server.metaproject.impl.MetaProjectImpl;
-import edu.stanford.smi.protege.server.metaproject.impl.MetaProjectInstanceImpl;
 import edu.stanford.smi.protege.server.metaproject.impl.OperationImpl;
-import edu.stanford.smi.protege.server.metaproject.impl.UserInstanceImpl;
+import edu.stanford.smi.protege.server.metaproject.impl.ProjectInstanceImpl;
+import edu.stanford.smi.protege.server.metaproject.impl.UserImpl;
 import edu.stanford.smi.protege.util.URIUtilities;
 
 public class Policy_Test extends SimpleTestCase {
   
   public static final String METAPROJECT = "junit/pprj/policy/metaproject.pprj";
   
-  public static final MetaProjectInstance PROJECT1 = new MetaProjectInstanceImpl("Newspaper");
+  public static final ProjectInstance PROJECT1 = new ProjectInstanceImpl("Newspaper");
   
-  public static final UserInstance        NATASHA  = new UserInstanceImpl("Natasha Noy");
-  public static final UserInstance        BOB      = new UserInstanceImpl("Bob");
-  public static final UserInstance        PAUL     = new UserInstanceImpl("Paul");
-  public static final UserInstance        DOUBLEO  = new UserInstanceImpl("Bond, James Bond");
+  public static final User        NATASHA  = new UserImpl("Natasha Noy");
+  public static final User        BOB      = new UserImpl("Bob");
+  public static final User        PAUL     = new UserImpl("Paul");
+  public static final User        DOUBLEO  = new UserImpl("Bond, James Bond");
   
   public static final Operation           RESTART  = new OperationImpl("RestartServer"); 
   public static final Operation     SELF_DESTRUCT  = new OperationImpl("DestroyInstallationToProtectAgents");
   public static final Operation           KILL     = new OperationImpl("LicensedKill");
   
   public static void checkAuthorization(Policy p,
-                                        UserInstance user,
+                                        User user,
                                         Operation op,
-                                        MetaProjectInstance project,
+                                        ProjectInstance project,
                                         boolean allowed) {
     Set<Operation> operations = p.getAllowedOperations(user, project);
     if (allowed) {
@@ -42,8 +41,7 @@ public class Policy_Test extends SimpleTestCase {
   
   public void testPolicy01() {
     MetaProject mp = new MetaProjectImpl(URIUtilities.createURI(METAPROJECT));
-    
-    
+        
     Policy p = mp.getPolicy();
 
     checkAuthorization(p, NATASHA, OperationImpl.WRITE, PROJECT1, true);
