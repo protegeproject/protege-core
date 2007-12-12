0294e3a39b0f316280090c16736787c820f1fe0d
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2007-12-12 22:37:00 +0000

    Cache map simplified to use the java 5 WeakHashMap

--- protege-core/trunk/src/edu/stanford/smi/protege/util/CacheMap.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/util/CacheMap.java
@@ -8,22 +8,19 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.WeakHashMap;
 
 /**
  * @author Ray Fergerson
  *
  * Description of this class
  */
-public class CacheMap<X,Y> {
+public class CacheMap<X,Y>  extends WeakHashMap<X,Y> {
     private static final int INIT_SIZE = 10007;
-    private Map<X,Reference<? extends Y>> keyToReferenceMap 
-      = new HashMap<X,Reference<? extends Y>>(INIT_SIZE);
-    private Map<Reference<? extends Y>,X> referenceToKeyMap 
-      = new HashMap<Reference<? extends Y>,X>(INIT_SIZE);
-    private ReferenceQueue<Y> referenceQueue = new ReferenceQueue<Y>();
     private int maxSize;
 
     public CacheMap(int maxSize) {
+        super();
         this.maxSize = maxSize;
     }
     
@@ -31,69 +28,25 @@ public class CacheMap<X,Y> {
         this(Integer.MAX_VALUE);
     }
 
-    private void pollQueue() {
-        Reference<? extends Y> ref;
-        while ((ref = referenceQueue.poll()) != null) {
-            X key = referenceToKeyMap.remove(ref);
-            keyToReferenceMap.remove(key);
-        }
-    }
 
-    public Y get(X key) {
-        pollQueue();
-        SoftReference<? extends Y> ref = (SoftReference<? extends Y>) keyToReferenceMap.get(key);
-        Y value = (ref == null) ? null : ref.get();
-        return value;
-    }
 
-    public void put(X key, Y value) {
-        pollQueue();
-        if (value == null) {
-            keyToReferenceMap.put(key, (Reference<Y>) null);
-        } else {
-            Reference<Y> reference 
-              = new SoftReference<Y>(value, referenceQueue);
-            keyToReferenceMap.put(key, reference);
-            referenceToKeyMap.put(reference, key);
-        }
+    public Y put(X key, Y value) {
+        Y oldValue = super.put(key, value);
         fixSize();
+        return oldValue;
     }
     
     private void fixSize() {
-        if (keyToReferenceMap.size() > maxSize) {
+        if (size() > maxSize) {
             Log.getLogger().info("removing elements from call cache");
             int count = 0;
             int nremove = maxSize/5;
-            Iterator<X> i = keyToReferenceMap.keySet().iterator();
+            Iterator<X> i = keySet().iterator();
             while (i.hasNext() && count++ < nremove) {
-                X key = i.next();
-                Reference<? extends Y> value = keyToReferenceMap.get(key);
-                referenceToKeyMap.remove(value);
+                i.next();
                 i.remove();
             }
         }
-    }
-
-    public void remove(X key) {
-        pollQueue();
-        Reference<? extends Y> reference = keyToReferenceMap.remove(key);
-        referenceToKeyMap.remove(reference);
-    }
-
-    public void clear() {
-        keyToReferenceMap.clear();
-        referenceToKeyMap.clear();
-        referenceQueue = new ReferenceQueue();
-    }
-
-    public Collection<X> getKeys() {
-        pollQueue();
-        return new ArrayList<X>(keyToReferenceMap.keySet());
-    }
-
-    public String toString() {
-        pollQueue();
-        return StringUtilities.getClassName(this);
     }
 
 }
