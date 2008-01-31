7c614e5292652458e6b0be2070df756b47b20f90
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-01-31 03:19:24 +0000

    no longer using the bad version of OperationImpl

--- protege-core/branches/namespace/junit/edu/stanford/smi/protege/server/metaproject/Policy_Test.java
+++ protege-core/branches/namespace/junit/edu/stanford/smi/protege/server/metaproject/Policy_Test.java
@@ -4,25 +4,33 @@ import java.util.Set;
 
 import edu.stanford.smi.protege.model.framestore.SimpleTestCase;
 import edu.stanford.smi.protege.server.metaproject.impl.MetaProjectImpl;
-import edu.stanford.smi.protege.server.metaproject.impl.OperationImpl;
-import edu.stanford.smi.protege.server.metaproject.impl.ProjectInstanceImpl;
-import edu.stanford.smi.protege.server.metaproject.impl.UserImpl;
+import edu.stanford.smi.protege.server.metaproject.impl.UnbackedOperationImpl;
 import edu.stanford.smi.protege.util.URIUtilities;
 
 public class Policy_Test extends SimpleTestCase {
   
   public static final String METAPROJECT = "junit/pprj/policy/metaproject.pprj";
   
-  public static final ProjectInstance PROJECT1 = new ProjectInstanceImpl("Newspaper");
+  public static final String PROJECT1 ="Newspaper";
   
-  public static final User        NATASHA  = new UserImpl("Natasha Noy");
-  public static final User        BOB      = new UserImpl("Bob");
-  public static final User        PAUL     = new UserImpl("Paul");
-  public static final User        DOUBLEO  = new UserImpl("Bond, James Bond");
+  public static final String        NATASHA  = "Natasha Noy";
+  public static final String        BOB      = "Bob";
+  public static final String        PAUL     = "Paul";
+  public static final String        DOUBLEO  = "Bond, James Bond";
   
-  public static final Operation           RESTART  = new OperationImpl("RestartServer"); 
-  public static final Operation     SELF_DESTRUCT  = new OperationImpl("DestroyInstallationToProtectAgents");
-  public static final Operation           KILL     = new OperationImpl("LicensedKill");
+  public static final Operation           RESTART  = new UnbackedOperationImpl("RestartServer", null); 
+  public static final Operation     SELF_DESTRUCT  = new UnbackedOperationImpl("DestroyInstallationToProtectAgents",null);
+  public static final Operation           KILL     = new UnbackedOperationImpl("LicensedKill",null);
+  
+  public static void checkAuthorization(Policy p,
+                                        String userName,
+                                        Operation op,
+                                        String projectName,
+                                        boolean allowed) {
+    User user = p.getUserByName(userName);
+    ProjectInstance project = p.getProjectInstanceByName(projectName);
+    checkAuthorization(p, user, op, project, allowed);
+  }
   
   public static void checkAuthorization(Policy p,
                                         User user,
@@ -44,13 +52,13 @@ public class Policy_Test extends SimpleTestCase {
         
     Policy p = mp.getPolicy();
 
-    checkAuthorization(p, NATASHA, OperationImpl.WRITE, PROJECT1, true);
-    checkAuthorization(p, BOB,     OperationImpl.WRITE, PROJECT1, false);
-    checkAuthorization(p, PAUL,    OperationImpl.WRITE, PROJECT1, false);
+    checkAuthorization(p, NATASHA, MetaProjectConstants.OPERATION_WRITE, PROJECT1, true);
+    checkAuthorization(p, BOB,     MetaProjectConstants.OPERATION_WRITE, PROJECT1, false);
+    checkAuthorization(p, PAUL,    MetaProjectConstants.OPERATION_WRITE, PROJECT1, false);
     checkAuthorization(p, PAUL,    KILL,                PROJECT1, false);
     checkAuthorization(p, PAUL,    RESTART,             PROJECT1, true);
     checkAuthorization(p, DOUBLEO, KILL,                PROJECT1, true);
     checkAuthorization(p, DOUBLEO, SELF_DESTRUCT,       PROJECT1, true);
-    checkAuthorization(p, DOUBLEO, OperationImpl.WRITE, PROJECT1, false);
+    checkAuthorization(p, DOUBLEO, MetaProjectConstants.OPERATION_WRITE, PROJECT1, false);
   }
 }
