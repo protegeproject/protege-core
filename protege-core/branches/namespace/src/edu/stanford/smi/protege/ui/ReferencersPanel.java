11012c7d84f680921adde5dbd135a4c583a5dd70
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-02-04 21:40:44 +0000

    java5

--- protege-core/branches/namespace/src/edu/stanford/smi/protege/ui/ReferencersPanel.java
+++ protege-core/branches/namespace/src/edu/stanford/smi/protege/ui/ReferencersPanel.java
@@ -89,11 +89,11 @@ public class ReferencersPanel extends SelectableContainer {
         model.addColumn("Facet");
         // model.addColumn("Kind");
         KnowledgeBase kb = instance.getKnowledgeBase();
-        ArrayList references = new ArrayList(kb.getReferences(instance, 1000));
+        ArrayList<Reference> references = new ArrayList<Reference>(kb.getReferences(instance, 1000));
         Collections.sort(references, new ReferenceComparator());
-        Iterator i = references.iterator();
+        Iterator<Reference> i = references.iterator();
         while (i.hasNext()) {
-            Reference ref = (Reference) i.next();
+            Reference ref = i.next();
             model.addRow(new Object[] { ref.getFrame(), ref.getSlot(), ref });
         }
         return model;
