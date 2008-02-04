029735f139e90a7e49ae61a3a152de6209c91709
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-02-04 21:41:56 +0000

    java5'd a Comparator

--- protege-core/branches/namespace/src/edu/stanford/smi/protege/ui/FrameComparator.java
+++ protege-core/branches/namespace/src/edu/stanford/smi/protege/ui/FrameComparator.java
@@ -1,25 +1,18 @@
 package edu.stanford.smi.protege.ui;
 
-import java.util.*;
+import java.util.Comparator;
 
-import edu.stanford.smi.protege.model.*;
-import edu.stanford.smi.protege.util.*;
+import edu.stanford.smi.protege.model.Frame;
+import edu.stanford.smi.protege.util.Log;
 
 /**
  * Comparator for frames.  The actually comparison criterion is delegated to the Frame.compareTo() method.
  *
  * @author    Ray Fergerson <fergerson@smi.stanford.edu>
  */
-public class FrameComparator implements Comparator {
+public class FrameComparator implements Comparator<Frame> {
 
-    public int compare(Object o1, Object o2) {
-        int result;
-        if (o1 instanceof Frame && o2 instanceof Frame) {
-            result = ((Frame) o1).compareTo(o2);
-        } else {
-            Log.getLogger().warning("Invalid types: " + o1 + " " + o2);
-            result = 0;
-        }
-        return result;
+    public int compare(Frame f1, Frame f2) {
+        return  f1.compareTo(f2);
     }
 }
