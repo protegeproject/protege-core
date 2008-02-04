68b76d4d147f1f9f165b2340d45930d8bca1e6a8
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-02-04 21:33:10 +0000

    new deletion undo

--- protege-core/branches/namespace/src/edu/stanford/smi/protege/model/framestore/undo/DeleteSlotCommand.java
+++ protege-core/branches/namespace/src/edu/stanford/smi/protege/model/framestore/undo/DeleteSlotCommand.java
@@ -1,31 +1,62 @@
 package edu.stanford.smi.protege.model.framestore.undo;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
-import edu.stanford.smi.protege.model.*;
-import edu.stanford.smi.protege.model.framestore.*;
+import edu.stanford.smi.protege.model.Cls;
+import edu.stanford.smi.protege.model.Frame;
+import edu.stanford.smi.protege.model.Slot;
+import edu.stanford.smi.protege.model.framestore.FrameStore;
 
-class DeleteSlotCommand extends SimpleCommand {
-    private Slot slot;
-    private FrameID id;
-    private Collection directTypes;
-    private Collection directSuperslots;
+public class DeleteSlotCommand extends DeleteFrameCommand {
+    
+    private Map<Frame, Collection> directOwnSlotValues = new HashMap<Frame, Collection>();
+    private Map<Cls, Collection> directTemplateSlotValues = new HashMap<Cls, Collection>();
 
-    DeleteSlotCommand(FrameStore delegate, Slot slot) {
-        super(delegate);
-        this.slot = slot;
-        this.id = slot.getFrameID();
-        this.directTypes = new ArrayList(slot.getDirectTypes());
-        this.directSuperslots = new ArrayList(slot.getDirectSuperslots());
-        setDescription("Delete slot " + getText(slot));
+    public DeleteSlotCommand(FrameStore delegate, Slot slot) {
+        super(delegate, slot);
     }
-    public Object doIt() {
-        getDelegate().deleteSlot(slot);
-        slot.markDeleted(true);
-        return null;
+
+    public Slot getSlot() {
+        return (Slot) getFrame();
     }
-    public void undoIt() {
-        getDelegate().createSlot(id, directTypes, directSuperslots, false);
-        slot.markDeleted(false);
+    
+    @Override
+    protected void saveFrame() {
+        super.saveFrame();
+        saveSlot();
+    }
+    
+    @Override
+    protected void restoreFrame() {
+        super.restoreFrame();
+        restoreSlot();
+    }
+    
+    private void saveSlot() {
+        Slot slot = getSlot();
+        for (Frame frame : getDelegate().getFramesWithAnyDirectOwnSlotValue(slot)) {
+            Collection values = getDelegate().getDirectOwnSlotValues(frame, slot);
+            if (values != null && !values.isEmpty()) {
+                directOwnSlotValues.put(frame, values);
+            }
+        }
+        for (Cls cls : getDelegate().getClsesWithAnyDirectTemplateSlotValue(slot)) {
+            Collection values = getDelegate().getDirectTemplateSlotValues(cls, slot);
+            if  (values != null && !values.isEmpty()) {
+                directTemplateSlotValues.put(cls, values);
+            }
+        }
+    }
+    
+    private  void restoreSlot() {
+        for (Entry<Frame, Collection> entry : directOwnSlotValues.entrySet()) {
+            getDelegate().setDirectOwnSlotValues(entry.getKey(), getSlot(), entry.getValue());
+        }
+        for (Entry<Cls, Collection> entry : directTemplateSlotValues.entrySet()) {
+            getDelegate().setDirectTemplateSlotValues(entry.getKey(), getSlot(), entry.getValue());
+        }
     }
 }
