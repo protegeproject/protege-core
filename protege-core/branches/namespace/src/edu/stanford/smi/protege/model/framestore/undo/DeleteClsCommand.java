25d2007c44e39f6873947c0021472ed4261c7f0c
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-02-04 21:32:52 +0000

    new deletion undo

--- protege-core/branches/namespace/src/edu/stanford/smi/protege/model/framestore/undo/DeleteClsCommand.java
+++ protege-core/branches/namespace/src/edu/stanford/smi/protege/model/framestore/undo/DeleteClsCommand.java
@@ -1,35 +1,69 @@
 package edu.stanford.smi.protege.model.framestore.undo;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
 
-import edu.stanford.smi.protege.model.*;
-import edu.stanford.smi.protege.model.framestore.*;
+import edu.stanford.smi.protege.model.Cls;
+import edu.stanford.smi.protege.model.Facet;
+import edu.stanford.smi.protege.model.Slot;
+import edu.stanford.smi.protege.model.framestore.FrameStore;
 
-class DeleteClsCommand extends SimpleCommand {
-    private Cls cls;
-    private FrameID id;
-    private Collection directTypes;
-    private Collection directSuperclasses;
-
-    DeleteClsCommand(FrameStore delegate, Cls cls) {
-        super(delegate);
-        this.cls = cls;
-        this.id = cls.getFrameID();
-        this.directTypes = new ArrayList(cls.getDirectTypes());
-        this.directSuperclasses = new ArrayList(cls.getDirectSuperclasses());
-        setDescription("Delete class " + getText(cls));
-        // Log.enter(this, "DeleteClsCommand", cls);
+public class DeleteClsCommand extends DeleteFrameCommand {
+    private Map<Slot, Collection> directTemplateSlotValues = new HashMap<Slot, Collection>();
+    private Map<Slot, Map<Facet, Collection>> directTemplateSlotFacetValues = new HashMap<Slot, Map<Facet, Collection>>();
+    
+    public DeleteClsCommand(FrameStore delegate, Cls cls) {
+        super(delegate, cls);
+    }
+    
+    public Cls getCls() {
+        return (Cls) getFrame();
+    }
+    
+    protected void saveFrame() {
+        super.saveFrame();
+        saveCls();
+    }
+    
+    protected void restoreFrame() {
+        super.restoreFrame();
+        restoreCls();
+    }
+    
+    private void saveCls() {
+        Cls cls = getCls();
+        Set<Slot> templateSlots = getDelegate().getTemplateSlots(cls);
+        for (Slot slot : templateSlots) {
+            Collection values = getDelegate().getDirectTemplateSlotValues(cls, slot);
+            if (values != null && !values.isEmpty()) {
+                directTemplateSlotValues.put(slot, values);
+            }
+            Map<Facet, Collection> facetMap = new HashMap<Facet, Collection>();
+            directTemplateSlotFacetValues.put(slot, facetMap);
+            Set<Facet> facets  = getDelegate().getTemplateFacets(cls, slot);
+            for (Facet facet : facets) {
+                Collection facetValues = getDelegate().getDirectTemplateFacetValues(cls, slot, facet);
+                if (facetValues != null && !facetValues.isEmpty()) {
+                    facetMap.put(facet, facetValues);
+                }
+            }
+        }
+    }
+    
+    private void restoreCls() {
+        Cls cls = getCls();
+        for (Entry<Slot, Collection>  entry : directTemplateSlotValues.entrySet()) {
+            getDelegate().setDirectTemplateSlotValues(cls, entry.getKey(), entry.getValue());
+        }
+        for (Entry<Slot, Map<Facet, Collection>> entry : directTemplateSlotFacetValues.entrySet()) {
+            Slot slot = entry.getKey();
+            for (Entry<Facet, Collection> facetEntry : entry.getValue().entrySet()) {
+                getDelegate().setDirectTemplateFacetValues(cls, slot, facetEntry.getKey(), facetEntry.getValue());
+            }
+        }
     }
 
-    public Object doIt() {
-        // Log.enter(this, "doIt", name);
-        getDelegate().deleteCls(cls);
-        cls.markDeleted(true);
-        return null;
-    }
-    public void undoIt() {
-        // Log.enter(this, "undoIt", name);
-        getDelegate().createCls(id, directTypes, directSuperclasses, false);
-        cls.markDeleted(false);
-    }
 }
