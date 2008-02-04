040715594a8020e9329f88cd8e3bbf9860ba8629
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-02-04 21:41:06 +0000

    java5'd a comparator

--- protege-core/branches/namespace/src/edu/stanford/smi/protege/ui/ReferenceComparator.java
+++ protege-core/branches/namespace/src/edu/stanford/smi/protege/ui/ReferenceComparator.java
@@ -1,19 +1,19 @@
 package edu.stanford.smi.protege.ui;
 
-import edu.stanford.smi.protege.model.*;
+import java.util.Comparator;
+
+import edu.stanford.smi.protege.model.Reference;
 
 /**
  * Comparator for instance of the {@link Reference} class.  The comparsion is done first on slots and then on frames.
  */
 
-public class ReferenceComparator extends FrameComparator {
+public class ReferenceComparator implements Comparator<Reference> {
 
-    public int compare(Object o1, Object o2) {
-        Reference r1 = (Reference) o1;
-        Reference r2 = (Reference) o2;
-        int result = super.compare(r1.getSlot(), r2.getSlot());
+    public int compare(Reference r1,  Reference r2) {
+        int result = r1.getSlot().compareTo(r2.getSlot());
         if (result == 0) {
-            result = super.compare(r1.getFrame(), r2.getFrame());
+            result = r1.getFrame().compareTo(r2.getFrame());
         }
         return result;
     }
