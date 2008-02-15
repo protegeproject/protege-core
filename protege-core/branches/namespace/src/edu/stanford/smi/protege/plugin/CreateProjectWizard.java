e52ffb1f24ea93f4cb76d6117c8d19228adfa5e1
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-02-15 20:41:08 +0000

    box was a little small for some of the dialogs

--- protege-core/branches/namespace/src/edu/stanford/smi/protege/plugin/CreateProjectWizard.java
+++ protege-core/branches/namespace/src/edu/stanford/smi/protege/plugin/CreateProjectWizard.java
@@ -19,7 +19,7 @@ public class CreateProjectWizard extends Wizard {
     public CreateProjectWizard(JComponent owner) {
         super(owner, "Create New Project");
         addPage(new SelectProjectTypeWizardPage(this));
-        setSize(new Dimension(400, 400));
+        setSize(new Dimension(400, 500));
     }
     
     public void setPlugin(CreateProjectPlugin plugin) {
