17c69c177c23331613d25d01f8819bc0b13aa780
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2008-05-16 04:01:03 +0000

    added a severity field to the message error

--- src/edu/stanford/smi/protege/util/MessageError.java
+++ src/edu/stanford/smi/protege/util/MessageError.java
@@ -7,8 +7,13 @@ package edu.stanford.smi.protege.util;
  *
  */
 public class MessageError {
+    public enum Severity {
+        WARNING, ERROR, FATAL;
+    }
+    
 	private Throwable exception;
 	private String message;
+	private Severity severity;
 	
 	public MessageError(String message) {
 		this(null, message);
@@ -19,8 +24,13 @@ public class MessageError {
 	}
 	
 	public MessageError(Throwable ex, String message) {
+	    this(ex,  message, Severity.WARNING);
+	}
+	
+	public MessageError(Throwable ex, String message, Severity severity) {
 		this.exception = ex;
 		this.message = message;
+		this.severity = severity;
 	}
 
 	public Throwable getException() {
@@ -37,6 +47,14 @@ public class MessageError {
 
 	public void setMessage(String message) {
 		this.message = message;
+	}
+	
+	public void setSeverity(Severity  severity) {
+	    this.severity = severity;
+	}
+	
+	public Severity getSeverity() {
+	    return severity;
 	}
 
 }
