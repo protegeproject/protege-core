35968591619a206d074e17db580bacc21a9abd00
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-06-23 01:20:58 +0000

    moved inappropriate interfaces to MetaProjectImpl

--- protege-core/trunk/src/edu/stanford/smi/protege/server/metaproject/MetaProject.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/server/metaproject/MetaProject.java
@@ -3,61 +3,45 @@ package edu.stanford.smi.protege.server.metaproject;
 import java.util.Collection;
 import java.util.Set;
 
-import edu.stanford.smi.protege.model.Cls;
-import edu.stanford.smi.protege.model.KnowledgeBase;
-import edu.stanford.smi.protege.model.Slot;
-
 public interface MetaProject {
-	public enum ClsEnum {
-		Project, User, Group, Operation, GroupOperation;
-	}
 
-	public enum SlotEnum {
-		name, password, location, group, member, allowedGroup, allowedOperation, allowedGroupOperation, owner, description, annotationProject;
-	}
+    Set<ProjectInstance> getProjects();
 
-	Cls getCls(ClsEnum cls);
+    ProjectInstance getProject(String name);
 
-	Slot getSlot(SlotEnum slot);
+    ProjectInstance createProject(String name);
 
-	
-	Set<ProjectInstance> getProjects();
 
-	ProjectInstance getProject(String name);
+    Set<User> getUsers();
 
-	
-	Set<User> getUsers();
-	
-	User getUser(String name);
+    User getUser(String name);
 
-	
-	Set<Operation> getOperations();
-	
-	Operation getOperation(String name);
-	
-	
-	Set<Group> getGroups();
-	
-	Group getGroup(String name);
+    User createUser(String name, String password);
 
-	
-	Policy getPolicy();
 
-	KnowledgeBase getKnowledgeBase();
+    Set<Operation> getOperations();
 
-	
-	ProjectInstance createProject(String name);
+    Operation getOperation(String name);
 
-	User createUser(String name, String password);
-	
-	Group createGroup(String name);
-	
-	Operation createOperation(String name);
-	
-	GroupOperation createGroupOperation();
+    Operation createOperation(String name);
 
-	
-	boolean save(Collection errors);
+
+    Set<Group> getGroups();
+
+    Group getGroup(String name);
+
+    Group createGroup(String name);
+
+
+    Set<GroupOperation> getGroupOperations();
+
+    GroupOperation createGroupOperation();
+
+    Policy getPolicy();
+
+    
+    @SuppressWarnings("unchecked")
+    boolean save(Collection errors);
 
 }
 
