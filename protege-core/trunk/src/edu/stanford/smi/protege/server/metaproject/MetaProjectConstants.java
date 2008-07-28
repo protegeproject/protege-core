9bde08c12aff6e7de06f7bf9fa641988f222f005
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-07-28 23:37:00 +0000

    added constant for operation to kill other users session

--- protege-core/trunk/src/edu/stanford/smi/protege/server/metaproject/MetaProjectConstants.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/server/metaproject/MetaProjectConstants.java
@@ -3,8 +3,8 @@ package edu.stanford.smi.protege.server.metaproject;
 import edu.stanford.smi.protege.server.metaproject.impl.UnbackedOperationImpl;
 
 public class MetaProjectConstants {
-	public final static Operation OPERATION_DISPLAY_IN_PROJECT_LIST = new  UnbackedOperationImpl("DisplayInProjectList", null);	
-    
+	public final static Operation OPERATION_DISPLAY_IN_PROJECT_LIST = new  UnbackedOperationImpl("DisplayInProjectList", null);
+
 	public final static Operation OPERATION_READ = new UnbackedOperationImpl("Read", null);
 	public final static Operation OPERATION_WRITE = new UnbackedOperationImpl("Write", null);
 
@@ -15,7 +15,9 @@ public class MetaProjectConstants {
 	public final static Operation OPERATION_ONTOLOGY_TAB_WRITE = new UnbackedOperationImpl("OntologyTabWrite", null);
 
 	public final static Operation OPERATION_CONFIGURE_SERVER = new UnbackedOperationImpl("ConfigureServer", null);
-	
+
+	public static final Operation OPERATION_KILL_OTHER_USER_SESSION = new UnbackedOperationImpl("KillOtherUserSession", null);
+
 	public static final String USER_WORLD = "World";
-	
+
 }
