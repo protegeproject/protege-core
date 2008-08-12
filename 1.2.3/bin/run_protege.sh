978f299adcaa28b8c648c656365c368827f1c3b3
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2008-08-12 03:37:47 +0000

    this version is only for nci debugging

--- 1.2.3/bin/run_protege.sh
+++ 1.2.3/bin/run_protege.sh
@@ -21,15 +21,15 @@ if [ ! -e $JAVA_PATH/java ]; then
 	exit 1
 fi
 
-JARS=protege.jar:driver.jar:driver1.jar:looks.jar:unicode_panel.jar
+JARS=protege.jar:mysql-connector-java-5.0.0-beta-bin.jar:looks.jar:unicode_panel.jar
 MAIN_CLASS=edu.stanford.smi.protege.Application
 MAXIMUM_MEMORY=-Xmx1900M
 OPTIONS=$MAXIMUM_MEMORY
 
 #Possible instrumentation options - debug, etc.
-#DEBUG_OPT="-agentlib:jdwp=transport=dt_socket,address=8100,server=y,suspend=n"
-#export LD_LIBRARY_PATH=/home/tredmond/dev/packages/yjp-7.5.6/bin/linux-x86-32
-#YJP_OPT="-agentlib:yjpagent=port=8142"
+DEBUG_OPT="-agentlib:jdwp=transport=dt_socket,address=8100,server=y,suspend=n"
+export LD_LIBRARY_PATH=/home/tredmond/dev/packages/yjp-7.5.6/bin/linux-x86-32
+YJP_OPT="-agentlib:yjpagent=port=8142"
 
 # Run Protege
 $JAVA_PATH/java $OPTIONS ${DEBUG_OPT} ${YJP_OPT} -cp $JARS $MAIN_CLASS $1
