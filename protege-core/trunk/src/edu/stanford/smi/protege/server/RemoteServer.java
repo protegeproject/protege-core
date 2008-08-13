6820da570126ea84bd672a17269437ebdea7f2cb
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-08-13 03:34:44 +0000

    added a few methods

--- protege-core/trunk/src/edu/stanford/smi/protege/server/RemoteServer.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/server/RemoteServer.java
@@ -5,28 +5,57 @@ import java.rmi.RemoteException;
 import java.util.Collection;
 
 import edu.stanford.smi.protege.model.KnowledgeBaseFactory;
+import edu.stanford.smi.protege.server.ServerProject.ProjectStatus;
+import edu.stanford.smi.protege.server.metaproject.Operation;
+import edu.stanford.smi.protege.util.ServerJob;
 
 public interface RemoteServer extends Remote {
     
     void reinitialize() throws RemoteException;
     
+    /*
+     * Session Management
+     */
     RemoteSession openSession(String username, String userMachine, String password) throws RemoteException;
     
     RemoteSession cloneSession(RemoteSession session) throws RemoteException;
     
     void closeSession(RemoteSession session) throws RemoteException;
-
-    Collection<String> getAvailableProjectNames(RemoteSession session) throws RemoteException;
-
+    
     Collection<RemoteSession> getCurrentSessions(String projectName, RemoteSession session) throws RemoteException;
 
+    /*
+     * Project Access
+     */
+    
+    Collection<String> getAvailableProjectNames(RemoteSession session) throws RemoteException;
+    
     RemoteServerProject openProject(String projectName, RemoteSession session) throws RemoteException;
+    
+    /*
+     * Server admin
+     */
     
     RemoteServerProject createProject(String projectName, RemoteSession session, KnowledgeBaseFactory kbfactory, boolean saveToMetaProject) throws RemoteException;
     
-    boolean allowsCreateUsers() throws RemoteException;
-   
+    void setProjectStatus(String projectName, ProjectStatus status, RemoteSession session);
+    
     boolean createUser(String userName, String password) throws RemoteException;
 
     void shutdown() throws RemoteException;
+    
+    void shutdown(String projectName, RemoteSession session);
+    
+    /*
+     * Policy
+     */
+    
+    boolean allowsCreateUsers() throws RemoteException;
+    
+    boolean isOperationAllowed(RemoteSession session, Operation op, String projectName);
+    
+    /* 
+     * Misc
+     */
+    Object executeServerJob(ServerJob job, RemoteSession session);
 }
