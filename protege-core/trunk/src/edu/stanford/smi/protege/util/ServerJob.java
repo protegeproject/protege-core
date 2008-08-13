e1ce8fa36227e807dbee2ec909f9fb104cc91e56
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-08-13 03:30:01 +0000

    the notion of a server job

---
+++ protege-core/trunk/src/edu/stanford/smi/protege/util/ServerJob.java
@@ -0,0 +1,44 @@
+package edu.stanford.smi.protege.util;
+
+import java.io.Serializable;
+
+import edu.stanford.smi.protege.exception.ProtegeException;
+import edu.stanford.smi.protege.model.DefaultKnowledgeBase;
+import edu.stanford.smi.protege.model.KnowledgeBase;
+import edu.stanford.smi.protege.model.Localizable;
+import edu.stanford.smi.protege.model.framestore.FrameStore;
+import edu.stanford.smi.protege.server.RemoteServer;
+import edu.stanford.smi.protege.server.RemoteSession;
+import edu.stanford.smi.protege.server.framestore.RemoteClientFrameStore;
+
+public abstract class ServerJob extends RemoteJob implements Serializable {
+    private static final long serialVersionUID = -7633748858975967471L;
+    
+    private RemoteServer server;
+    private RemoteSession session;
+
+    public ServerJob(RemoteServer server, RemoteSession session) {
+        this.server = server;
+        this.session = session;
+    }
+    
+    public ServerJob(KnowledgeBase kb) {
+        DefaultKnowledgeBase dkb = (DefaultKnowledgeBase) kb;
+        FrameStore terminalFrameStore = dkb.getTerminalFrameStore();
+        if (terminalFrameStore instanceof RemoteClientFrameStore) {
+          RemoteClientFrameStore clientFrameStore = (RemoteClientFrameStore) terminalFrameStore;
+          
+          server = clientFrameStore.getRemoteServer();
+          session = clientFrameStore.getSession();
+        }
+    }
+    
+    public RemoteSession getSession() {
+        return session;
+    }
+    
+    public Object execute() throws ProtegeException {
+        return server.executeServerJob(this, session);
+    }
+    
+}
