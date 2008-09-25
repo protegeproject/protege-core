bd92f200b452a941301c2d9fcbd4d75c10aeb2d5
msmith <msmith@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-09-25 14:14:54 +0000

    DatabaseFrameDB made into and interface so multiple, alternative implementations can be supported.

---
+++ src/edu/stanford/smi/protege/storage/database/DatabaseFrameDbFactory.java
@@ -0,0 +1,7 @@
+package edu.stanford.smi.protege.storage.database;
+
+public class DatabaseFrameDbFactory {
+	public static DatabaseFrameDb createDatabaseFrameDb() {
+		return new DefaultDatabaseFrameDb();
+	}
+}
