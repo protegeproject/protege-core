699971cbc914f70a0d81c29038dac51165769c73
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-10-08 20:40:28 +0000

    add the :NAME slot in the MultiNameSlot combo list, if not already there

--- protege-core/trunk/src/edu/stanford/smi/protege/ui/MultiSlotPanel.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/ui/MultiSlotPanel.java
@@ -5,8 +5,10 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+
 import javax.swing.ComboBoxModel;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
@@ -14,8 +16,9 @@ import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-import edu.stanford.smi.protege.model.Cls;
 import edu.stanford.smi.protege.model.BrowserSlotPattern;
+import edu.stanford.smi.protege.model.Cls;
+import edu.stanford.smi.protege.model.Slot;
 import edu.stanford.smi.protege.util.ComponentFactory;
 
 public class MultiSlotPanel extends JPanel {
@@ -33,8 +36,16 @@ public class MultiSlotPanel extends JPanel {
     private void createUI() {
     	this.setLayout(new GridBagLayout());
     	GridBagConstraints c = new GridBagConstraints();
-        Collection slots = cls.getVisibleTemplateSlots();
-    	
+        List<Slot> slots = new ArrayList<Slot>(cls.getVisibleTemplateSlots());
+        //add :NAME slot if not already there
+        Slot nameSlot = cls.getKnowledgeBase().getNameSlot();
+        if (!slots.contains(nameSlot)) {
+        	slots.add(nameSlot);
+        }
+
+        //sort
+        Collections.sort(slots, new FrameComparator());
+
     	// label
         c.gridx = 0;
     	c.gridy = 0;
@@ -42,7 +53,7 @@ public class MultiSlotPanel extends JPanel {
     	c.insets = new Insets(2, 2, 8, 2);
     	c.anchor = GridBagConstraints.FIRST_LINE_START;
     	add(ComponentFactory.createLabel("Set display slots and optional text:"), c);
-    	
+
     	// first row
     	c.gridx = 0;
     	c.gridy = 1;
@@ -51,7 +62,7 @@ public class MultiSlotPanel extends JPanel {
     	c.insets = new Insets(2, 2, 2, 2);
     	c.weightx = 0.5;
     	add(createTextPanel(), c);
-    	
+
     	// all other rows
     	for (int i=1; i<=4; i++) {
     		c.gridy = i;
@@ -77,7 +88,7 @@ public class MultiSlotPanel extends JPanel {
         panels.add(slotBox);
         return slotBox;
     }
-    
+
     private void addComponentRow(GridBagConstraints c, Collection slots) {
     	c.gridx = 1;
     	c.weightx = 1.0;
@@ -86,11 +97,11 @@ public class MultiSlotPanel extends JPanel {
     	c.gridx = 2;
     	c.weightx = 0.5;
     	add(createTextPanel(), c);
-    	
+
     	c.gridx = 3;
     	c.weightx = 1.0;
     	add(createSlotPanel(slots), c);
-    	
+
     	c.gridx = 4;
     	c.weightx = 0.5;
     	add(createTextPanel(), c);
@@ -119,7 +130,7 @@ public class MultiSlotPanel extends JPanel {
             }
         }
     }
-    
+
     public BrowserSlotPattern getBrowserTextPattern() {
         List elements = new ArrayList();
         Iterator i = panels.iterator();
