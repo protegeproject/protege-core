4c36c241455a479f852e2c7313a45e5332f0bfb0
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2008-11-17 01:37:59 +0000

    A new implementation based in ZipInput/OutputStreams.  The finish() method recommended by the o'reilly book didn't work because you are not supposed to write after a finish.  I now flush by closing and starting a new  ZipEntry

--- src/edu/stanford/smi/protege/server/socket/CompressingInputStream.java
+++ src/edu/stanford/smi/protege/server/socket/CompressingInputStream.java
@@ -2,38 +2,73 @@ package edu.stanford.smi.protege.server.socket;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.zip.GZIPInputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import edu.stanford.smi.protege.util.Log;
 
 public class CompressingInputStream extends InputStream {
-    private InputStream original;
-    private GZIPInputStream compressed;
+    private static transient final Logger  log  = Log.getLogger(CompressingInputStream.class);
+    private ZipInputStream compressing;
+    private ZipEntry entry;
+    boolean initialized = false;
     
-    public CompressingInputStream(InputStream original) {
-        this.original = original;
+    public CompressingInputStream(InputStream is) {
+        compressing = new ZipInputStream(is);
     }
-    
 
     @Override
     public int read() throws IOException {
-        if (compressed == null) {
-            compressed = new GZIPInputStream(original);
+        if (!initialize() || entry == null) {
+            return  -1;
         }
-        return compressed.read();
+        int ret = compressing.read();
+        if (ret < 0) {
+            compressing.closeEntry();
+            if ((entry = compressing.getNextEntry()) != null) {
+                if (log.isLoggable(Level.FINER)) {
+                    log.finer("InputStream: reading new segment " + entry.getName());
+                }
+                ret = compressing.read();
+            }
+        }
+        return ret;
     }
     
     @Override
-    public int available() throws IOException {
-        return compressed.available();
+    public int read(byte[] b, int off, int len) throws IOException {
+        if (!initialize() || entry == null) {
+            return  -1;
+        }
+        int bytesRead = compressing.read(b, off, len);
+        if (bytesRead < 0) {
+            compressing.closeEntry();
+            if  ((entry = compressing.getNextEntry()) != null) {
+                if (log.isLoggable(Level.FINER)) {
+                    log.finer("InputStream: reading new segment " + entry.getName());
+                }
+                bytesRead = compressing.read(b, off, len);
+            }
+        }
+        return bytesRead;
     }
-    
+ 
     @Override
     public void close() throws IOException {
-        if (compressed != null) {
-            compressed.close();
+        if (entry != null)  {
+            compressing.closeEntry();
         }
-        else {
-            original.close();
-        }
+        compressing.close();
     }
-
+    
+    private boolean initialize() throws IOException {
+        if (!initialized) {
+            initialized = true;
+            entry = compressing.getNextEntry();
+            return entry != null;
+        }
+        return true;
+    }
 }
--- src/edu/stanford/smi/protege/server/socket/CompressingOutputStream.java
+++ src/edu/stanford/smi/protege/server/socket/CompressingOutputStream.java
@@ -2,42 +2,79 @@ package edu.stanford.smi.protege.server.socket;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.zip.GZIPOutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import edu.stanford.smi.protege.util.Log;
 
 public class CompressingOutputStream extends OutputStream {
-    private OutputStream original;
-    private GZIPOutputStream compressed;
+    private static final transient Logger log = Log.getLogger(CompressingOutputStream.class);
+    public final static int BUFFER_SIZE = 4096;
     
-    public CompressingOutputStream(OutputStream original) {
-        this.original = original;
+    private byte[] data = new byte[BUFFER_SIZE];
+    int offset = 0;  // the next location in the buffer to write to
+                     // also doubles as the size of the unflushed data
+    
+    private OutputStream os;
+    private ZipOutputStream compressing;
+    private static int blockCounter = 0;
+
+    public CompressingOutputStream(OutputStream os) {
+        this.os = os;
+        compressing = new ZipOutputStream(os);
     }
+    
 
     @Override
     public void write(int b) throws IOException {
-        if (compressed == null) {
-            compressed = new GZIPOutputStream(original);
-        }
-        compressed.write(b);
-        return;
+        ensureNotFull();
+        data[offset++] = (byte) b;
+        ensureNotFull();
     }
     
     @Override
-    public void close() throws IOException {
-        if (compressed != null) {
-            compressed.close();
+    public void write(byte[] b, int off, int len) throws IOException {
+        while (len > 0) {
+            ensureNotFull();
+            for (; len > 0 && offset < BUFFER_SIZE; offset++, off++, len--) {
+                data[offset] = b[off];
+            }
         }
-        else {
-            original.close();
-        }
+        ensureNotFull();
     }
     
     @Override
     public void flush() throws IOException {
-        if (compressed != null) {
-            compressed.finish();
+        if (offset > 0) {
+            if (log.isLoggable(Level.FINER)) {
+                log.finer("OutputStream: Flushing output by starting new segment " + (blockCounter + 1));
+            }
+            ZipEntry entry = new ZipEntry("Segment" + blockCounter++);
+            entry.setMethod(ZipEntry.DEFLATED);
+            entry.setSize(offset);
+            compressing.putNextEntry(entry);
+            compressing.write(data, 0, offset);
+            compressing.closeEntry();
+            if (log.isLoggable(Level.FINER)) {
+                log.finer("OutputStream: segment " + blockCounter + " written (" + offset + " bytes)");
+            }
         }
-        else {
-            original.flush();
+        offset = 0;
+        os.flush();
+    }
+    
+    @Override
+    public void close() throws IOException {
+        flush();
+        compressing.close();
+    }
+    
+    
+    private void ensureNotFull() throws IOException {
+        if (offset >= BUFFER_SIZE) {
+            flush();
         }
     }
 
