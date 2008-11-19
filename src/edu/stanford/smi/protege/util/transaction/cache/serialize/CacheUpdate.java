c8b134beddfcb3a6596fdf3be88b6ccc53873474
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2008-11-19 06:39:38 +0000

    By definition these classes need to be serializable

--- src/edu/stanford/smi/protege/util/transaction/cache/serialize/CacheUpdate.java
+++ src/edu/stanford/smi/protege/util/transaction/cache/serialize/CacheUpdate.java
@@ -1,7 +1,10 @@
 package edu.stanford.smi.protege.util.transaction.cache.serialize;
 
-public class CacheUpdate<S, V, R> {
-	private S session;
+import java.io.Serializable;
+
+public class CacheUpdate<S, V, R> implements Serializable {
+    private static final long serialVersionUID = 6402530119072810497L;
+    private S session;
 	
 	public CacheUpdate(S session) {
 	    this.session = session;
