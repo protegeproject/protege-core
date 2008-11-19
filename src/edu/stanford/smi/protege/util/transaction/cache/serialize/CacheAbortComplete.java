603b30080c60bf6963fc438140ebaa1cec81f2da
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2008-11-19 07:30:31 +0000

    added the notion of performing an update

--- src/edu/stanford/smi/protege/util/transaction/cache/serialize/CacheAbortComplete.java
+++ src/edu/stanford/smi/protege/util/transaction/cache/serialize/CacheAbortComplete.java
@@ -1,7 +1,14 @@
 package edu.stanford.smi.protege.util.transaction.cache.serialize;
 
+import edu.stanford.smi.protege.util.transaction.cache.Cache;
+
 public class CacheAbortComplete<S,V,R> extends SerializedCacheUpdate<S,V,R> {
     public CacheAbortComplete() {
         super(null);
+    }
+    
+    @Override
+    public void performUpdate(Cache<S, V, R> cache) {
+        cache.abortCompleteCache();
     }
 }
