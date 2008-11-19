8c95745e33e7571e40a62c9d2cde2f43e53faf37
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-11-19 07:30:31 +0000

    added the notion of performing an update

--- protege-core/trunk/src/edu/stanford/smi/protege/util/transaction/cache/serialize/CacheStartComplete.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/util/transaction/cache/serialize/CacheStartComplete.java
@@ -1,9 +1,16 @@
 package edu.stanford.smi.protege.util.transaction.cache.serialize;
 
+import edu.stanford.smi.protege.util.transaction.cache.Cache;
+
 public class CacheStartComplete<S, V, R> extends SerializedCacheUpdate<S, V, R> {
     
     public CacheStartComplete() {
         super(null);
+    }
+    
+    @Override
+    public void performUpdate(Cache<S, V, R> cache) {
+        cache.startCompleteCache();
     }
 
 }
