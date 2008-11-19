10362cee7a40d125878c2d5e65ce21633cbbf92f
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2008-11-19 07:36:19 +0000

    merged with trunk to revision 12211 - performUpdate code

--- src/edu/stanford/smi/protege/util/transaction/cache/serialize/CacheCommitTransaction.java
+++ src/edu/stanford/smi/protege/util/transaction/cache/serialize/CacheCommitTransaction.java
@@ -1,8 +1,15 @@
 package edu.stanford.smi.protege.util.transaction.cache.serialize;
 
+import edu.stanford.smi.protege.util.transaction.cache.Cache;
+
 public class CacheCommitTransaction<S, V, R> extends SerializedCacheUpdate<S, V, R> {
 
     public CacheCommitTransaction(S session) {
         super(session);
+    }
+    
+    @Override
+    public void performUpdate(Cache<S, V, R> cache) {
+        cache.commitTransaction(getSession());
     }
 }
