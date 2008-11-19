b61dac969d60bcb87182bc5d8dd7e72f438e8e36
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-11-19 06:34:43 +0000

    serialized versions of the missing interfaces

---
+++ protege-core/trunk/src/edu/stanford/smi/protege/util/transaction/cache/serialize/CacheAbortComplete.java
@@ -0,0 +1,7 @@
+package edu.stanford.smi.protege.util.transaction.cache.serialize;
+
+public class CacheAbortComplete<S,V,R> extends CacheUpdate<S,V,R> {
+    public CacheAbortComplete() {
+        super(null);
+    }
+}
---
+++ protege-core/trunk/src/edu/stanford/smi/protege/util/transaction/cache/serialize/CacheStartComplete.java
@@ -0,0 +1,9 @@
+package edu.stanford.smi.protege.util.transaction.cache.serialize;
+
+public class CacheStartComplete<S, V, R> extends CacheUpdate<S, V, R> {
+    
+    public CacheStartComplete() {
+        super(null);
+    }
+
+}
