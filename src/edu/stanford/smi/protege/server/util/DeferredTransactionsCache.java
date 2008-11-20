141f30c1432bd676135696b3c509808dbac82601
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2008-11-20 01:19:11 +0000

    a trick to avoid havng to apply begin/ent/rolback transaction updates to all frame caches

---
+++ src/edu/stanford/smi/protege/server/util/DeferredTransactionsCache.java
@@ -0,0 +1,103 @@
+package edu.stanford.smi.protege.server.util;
+
+import java.util.List;
+
+import edu.stanford.smi.protege.model.framestore.Sft;
+import edu.stanford.smi.protege.server.RemoteSession;
+import edu.stanford.smi.protege.util.transaction.cache.Cache;
+import edu.stanford.smi.protege.util.transaction.cache.CacheResult;
+import edu.stanford.smi.protege.util.transaction.cache.serialize.SerializedCacheUpdate;
+
+public class DeferredTransactionsCache implements
+		Cache<RemoteSession, Sft, List> {
+	private List<SerializedCacheUpdate<RemoteSession, Sft, List>> transactionUpdates;
+	private int location = 0;
+	private Cache<RemoteSession, Sft, List> delegate;
+	
+	public DeferredTransactionsCache(Cache<RemoteSession, Sft, List> delegate, 
+			List<SerializedCacheUpdate<RemoteSession, Sft, List>> transactionUpdates) {
+		this.delegate = delegate;
+		this.transactionUpdates = transactionUpdates;
+	}
+	
+	public CacheResult<List> readCache(RemoteSession session, Sft var) {
+		catchUp();
+		return delegate.readCache(session, var);
+	}
+	
+	public void updateCache(RemoteSession session, Sft var) {
+		catchUp();
+		delegate.updateCache(session, var);
+	}
+	
+	public void updateCache(RemoteSession session, Sft var, List value) {
+		catchUp();
+		delegate.updateCache(session, var, value);
+	}
+	
+	public void modifyCache(RemoteSession session, Sft var) {
+		catchUp();
+		delegate.modifyCache(session, var);
+	}
+	
+	public void modifyCache(RemoteSession session, Sft var, List value) {
+		catchUp();
+		delegate.modifyCache(session, var, value);
+	}
+	
+	public void delete(RemoteSession session) {
+		catchUp();
+		delegate.delete(session);
+	}
+	
+	public void flush() {
+		while (deferredTransactions.read() != null) {
+			;
+		}
+		flush();
+	}
+	
+	public void startCompleteCache() {
+		catchUp();
+		delegate.startCompleteCache();
+	}
+	
+	public void finishCompleteCache() {
+		catchUp();
+		delegate.finishCompleteCache();
+	}
+	
+	public void abortCompleteCache() {
+		catchUp();
+		delegate.abortCompleteCache();
+	}
+	
+	public boolean isCacheComplete() {
+		catchUp();
+		return delegate.isCacheComplete();
+	}
+	
+	public void beginTransaction(RemoteSession session) {
+		throw new UnsupportedOperationException("transactions are deferred");
+	}
+	
+	public void commitTransaction(RemoteSession session) {
+		throw new UnsupportedOperationException("transactions are deferred");
+	}
+	
+	public void rollbackTransaction(RemoteSession session) {
+		throw new UnsupportedOperationException("transactions are deferred");
+	}
+	
+	public int getTransactionNesting(RemoteSession session) {
+		catchUp();
+		return delegate.getTransactionNesting(session);
+	}
+	
+	private void catchUp() {
+		while (transactionUpdates.size() > location) {
+			transactionUpdates.get(location++).performUpdate(delegate);
+		}
+	}
+
+}
