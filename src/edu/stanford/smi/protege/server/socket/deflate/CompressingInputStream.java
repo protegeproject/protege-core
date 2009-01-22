0fae48c03fc8e3b4b28cde0473aeb060a9f0a02d
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2009-01-22 18:51:16 +0000

    refactored so that it is easier to extend

--- src/edu/stanford/smi/protege/server/socket/deflate/CompressingInputStream.java
+++ src/edu/stanford/smi/protege/server/socket/deflate/CompressingInputStream.java
@@ -10,13 +10,37 @@ import java.util.zip.Inflater;
 
 import edu.stanford.smi.protege.util.Log;
 
+/**
+ * This code is based on the ideas presented in {@link http://javatechniques.com/blog/compressing-data-sent-over-a-socket/} by 
+ * Philip Isenhour.  I am very grateful for the approach that he presented.  The key idea is to avoid using the GZip and the Zip streams
+ * and to use the Deflater and Inflater methods directly.  In addition, Philip Isenhour essentially defines a packet type that has a 
+ * header indicating the size and compressed size of the packet contents.  I took these two key ideas and wrote the following code 
+ * by scratch without reference to Philip Isenhour's documents.  I think that some version of Philip Isenhour's ideas should find their way 
+ * into the core java libraries because otherwise people will continue struggling with this problem.
+ * <p>
+ * I have tried several other approaches to a compressing input and compressing output stream.  The first approach was to base the 
+ * input and output streams on the GZip input and output stream.  There are web pages on the internet that suggest that calling the GZipOutputStream's
+ * finish() method during the flush() will work.  I had trouble with this approach when a write occurs on the stream after
+ * the flush() (which calls finish()). I would get exceptions indicating that the GZip Output stream was finished and therefore unwriteable.
+ * <p>
+ * I then tried to use the ZIPInput/OutputStreams.  I would flush data by creating a ZipEntry and writing it out.  This approach
+ * actually worked very well.  But it had a mysterious bug where some data was either not fully written out or not read. In the rmi
+ * context things would hang.  This bug was relatively rare and only happened on certain machines.  I never found out what the problem was.
+ * <p>
+ * The beauty of Philip Isenhour's approach is that the developer can completely control how data is flushed and fully written out.  The developer
+ * can also ensure that on the read method all the data is fully read.  So there should not be any more rmi hangs.  The only issue is whether the 
+ * deflate/inflate logic is correct.  This is pretty thoroughly tested in our server-client testing (though there are *always* bugs hidden somewhere).
+ * 
+ * @author tredmond
+ *
+ */
 public class CompressingInputStream extends InputStream {
     private static Logger log = Log.getLogger(CompressingInputStream.class);
     
-    private InputStream is;
+    protected InputStream is;
     
-    private byte buffer[];
-    private int offset;
+    protected byte buffer[];
+    protected int offset;
     
     private Inflater inflater;
     
@@ -34,7 +58,7 @@ public class CompressingInputStream extends InputStream {
     @Override
     public int read() throws IOException {
         if (buffer == null) {
-            readBuffer();
+            fillBuffer();
         }
         if (buffer == null) {
             return -1;
@@ -49,7 +73,7 @@ public class CompressingInputStream extends InputStream {
     @Override
     public int read(byte[] b, int off, int len) throws IOException {
         if (buffer == null) {
-            readBuffer();
+            fillBuffer();
         }
         if (buffer == null) {
             return -1;
@@ -64,27 +88,22 @@ public class CompressingInputStream extends InputStream {
         return bytesRead;
     }
     
-    public void readBuffer() throws IOException {
-        int size = offset;
+    private void fillBuffer() throws IOException {
         buffer = null;
         offset = 0;
-        
+        PacketHeader header = PacketHeader.read(is);
+        buffer = new byte[header.getSize()];
+        fillBuffer(header);
+    }
+    
+    protected void fillBuffer(PacketHeader header) throws IOException {
         inflater.reset();
         
-        PacketHeader header = PacketHeader.read(is);
         int compressedSize = header.getCompressedSize();
-        buffer = new byte[header.getSize()];
         byte compressedBuffer[] = new byte[compressedSize];
-              
-        int bytesRead = 0;
-        while (bytesRead < compressedSize) {
-            int readThisTime = is.read(compressedBuffer, bytesRead, compressedSize - bytesRead);
-            if (readThisTime == -1) {
-                throw new EOFException("Unabled to read entire compressed packet contents");
-            }
-            bytesRead += readThisTime;
-        }
 
+        readFully(compressedBuffer, compressedSize);
+        
         inflater.setInput(compressedBuffer);
         try {
             int inflatedSize = inflater.inflate(buffer);
@@ -107,7 +126,18 @@ public class CompressingInputStream extends InputStream {
         logPacket(compressedBuffer);
     }
     
-    private void logPacket(byte [] compressedBuffer) {
+    protected void readFully(byte[] b, int len) throws IOException {
+        int bytesRead = 0;
+        while (bytesRead < len) {
+            int readThisTime = is.read(b, bytesRead, len - bytesRead);
+            if (readThisTime == -1) {
+                throw new EOFException("Unabled to read entire compressed packet contents");
+            }
+            bytesRead += readThisTime;
+        }
+    }
+    
+    protected void logPacket(byte [] compressedBuffer) {
         if (!log.isLoggable(Level.FINEST)) {
             return;
         }
