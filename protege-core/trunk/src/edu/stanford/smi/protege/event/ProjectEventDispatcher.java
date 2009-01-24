7aa74c074fe26e3f7ee4bed0b973aa02d16e6da2
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2009-01-24 03:46:25 +0000

    work for server admin panel - added server session lost

--- protege-core/trunk/src/edu/stanford/smi/protege/event/ProjectEventDispatcher.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/event/ProjectEventDispatcher.java
@@ -1,10 +1,12 @@
 package edu.stanford.smi.protege.event;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Iterator;
 
-import edu.stanford.smi.protege.model.*;
-import edu.stanford.smi.protege.util.*;
-import edu.stanford.smi.protege.widget.*;
+import edu.stanford.smi.protege.model.Project;
+import edu.stanford.smi.protege.util.Assert;
+import edu.stanford.smi.protege.util.EventDispatcher;
+import edu.stanford.smi.protege.widget.ClsWidget;
 
 /**
  * Dispatcher to send project events to their listeners. 
@@ -31,6 +33,9 @@ public class ProjectEventDispatcher implements EventDispatcher {
                 case ProjectEvent.RUNTIME_CLS_WIDGET_CREATED:
                     listener.runtimeClsWidgetCreated(event);
                     break;
+                case ProjectEvent.SERVER_SESSION_LOST:
+                	listener.serverSessionLost(event);
+                	break;
                 default:
                     Assert.fail("bad type: " + type);
                     break;
