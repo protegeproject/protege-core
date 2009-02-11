1a8135726837fdaee43f04843887d7fbaf01b8c5
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2009-02-11 20:53:21 +0000

    Merging the changes to the changes to the cache utilities.     cd to the trunk protege-core directory     d=src/edu/stanford/smi/protege/util/transaction/cache     pushd $d     svn update     svn status     svn merge $p/trunk/$d $p/branches/cache/$d     svn diff -- looks right     popd     ant clean jar     svn status     svn commit

--- src/edu/stanford/smi/protege/util/transaction/cache/CacheFactory.java
+++ src/edu/stanford/smi/protege/util/transaction/cache/CacheFactory.java
@@ -5,7 +5,7 @@ import edu.stanford.smi.protege.util.transaction.TransactionIsolationLevel;
 import edu.stanford.smi.protege.util.transaction.TransactionMonitor;
 import edu.stanford.smi.protege.util.transaction.cache.impl.BasicCache;
 import edu.stanford.smi.protege.util.transaction.cache.impl.CompleteableCache;
-import edu.stanford.smi.protege.util.transaction.cache.impl.DeletableCache;
+import edu.stanford.smi.protege.util.transaction.cache.impl.InvalidatableCache;
 import edu.stanford.smi.protege.util.transaction.cache.impl.ReadCommittedCache;
 import edu.stanford.smi.protege.util.transaction.cache.impl.ReadUncommittedCache;
 import edu.stanford.smi.protege.util.transaction.cache.impl.RepeatableReadCache;
@@ -27,18 +27,18 @@ public class CacheFactory {
     public static <S, V, R> Cache<S, V, R> createEmptyCache(TransactionIsolationLevel level) {
         Cache<S, V, R> untransactedCache = new CompleteableCache<S, V, R>(new BasicCache<S, V, R>());
         if (level == TransactionIsolationLevel.NONE) {
-            return new DeletableCache<S, V, R>(untransactedCache, true);
+            return new InvalidatableCache<S, V, R>(untransactedCache, true);
         }
         if (level.compareTo(TransactionIsolationLevel.READ_UNCOMMITTED) <= 0) {
-            return new DeletableCache<S, V, R>(new ReadUncommittedCache<S, V, R>(untransactedCache), true);
+            return new InvalidatableCache<S, V, R>(new ReadUncommittedCache<S, V, R>(untransactedCache), true);
         }
         Cache<S, V, R> readCommittedCache = new ReadCommittedCache<S, V, R>(untransactedCache);
         if (level == TransactionIsolationLevel.READ_COMMITTED) {
-            return new DeletableCache<S, V, R>(readCommittedCache, false);
+            return new InvalidatableCache<S, V, R>(readCommittedCache, false);
         }
         else {
             RepeatableReadCache<S, V, R> repeatableReadCache = new RepeatableReadCache<S, V, R>(readCommittedCache);
-            return new DeletableCache<S, V, R>(repeatableReadCache, false);
+            return new InvalidatableCache<S, V, R>(repeatableReadCache, false);
         }
     }
 
--- src/edu/stanford/smi/protege/util/transaction/cache/impl/RepeatableReadCache.java
+++ src/edu/stanford/smi/protege/util/transaction/cache/impl/RepeatableReadCache.java
@@ -10,7 +10,6 @@ import edu.stanford.smi.protege.util.transaction.cache.CacheResult;
 
 public class RepeatableReadCache<S, V, R> implements Cache<S, V, R> {
     private Map<S, Set<V>> readOrWrittenVarsMap = new  HashMap<S, Set<V>>();
-    private Map<S, Map<V, CacheResult<R>>> transactedReads = new HashMap<S, Map<V, CacheResult<R>>>();
     private Cache<S, V, R> delegate;
     
     public RepeatableReadCache(Cache<S, V, R> delegate) {
@@ -18,15 +17,9 @@ public class RepeatableReadCache<S, V, R> implements Cache<S, V, R> {
     }
 
     public CacheResult<R> readCache(S session, V var) {
-        Set<V> readOrWrittenVars = readOrWrittenVarsMap.get(session);
-        Map<V, CacheResult<R>> reads = transactedReads.get(session);
-        int transactionNesting = delegate.getTransactionNesting(session);
-        
-        if (transactionNesting > 0 && !readOrWrittenVars.contains(var)) {
+        Set<V> readOrWrittenVars = readOrWrittenVarsMap.get(session);       
+        if (readOrWrittenVars != null && !readOrWrittenVars.contains(var)) {
             return new CacheResult<R>(null, false);
-        }
-        else if (transactionNesting > 0 && reads.containsKey(var)) {
-            return reads.get(var);
         }
         else {
             return delegate.readCache(session, var);
@@ -34,33 +27,22 @@ public class RepeatableReadCache<S, V, R> implements Cache<S, V, R> {
     }
 
     public void updateCache(S session, V var) {
-        if (getTransactionNesting(session) == 0) {
-            delegate.updateCache(session, var);
-        }
+        markReadOrWritten(session, var);
+        delegate.updateCache(session, var);
     }
 
     public void updateCache(S session, V var, R value) {
-        int transactionNesting = delegate.getTransactionNesting(session);
-        if (transactionNesting > 0) {
-            readOrWrittenVarsMap.get(session).add(var);
-            transactedReads.get(session).put(var, new CacheResult<R>(value, true));
-        }
-        else {
-            delegate.updateCache(session, var, value);
-        }
+        markReadOrWritten(session, var);
+        delegate.updateCache(session, var, value);
     }
 
     public void modifyCache(S session, V var) {
-        if (getTransactionNesting(session) > 0) {
-            readOrWrittenVarsMap.get(session).add(var);
-        }
+        markReadOrWritten(session, var);
         delegate.modifyCache(session, var);
     }
 
     public void modifyCache(S session, V var, R value) {
-        if (getTransactionNesting(session) > 0) {
-            readOrWrittenVarsMap.get(session).add(var);
-        }
+        markReadOrWritten(session, var);
         delegate.modifyCache(session, var, value);
     }
 
@@ -91,47 +73,40 @@ public class RepeatableReadCache<S, V, R> implements Cache<S, V, R> {
     public void beginTransaction(S session) {
         if (getTransactionNesting(session) == 0) {
             readOrWrittenVarsMap.put(session, new HashSet<V>());
-            transactedReads.put(session, new HashMap<V, CacheResult<R>>());
         }
         delegate.beginTransaction(session);
     }
 
     public void commitTransaction(S session) {
-        if (getTransactionNesting(session) <= 0) {
-            flush();
-            return;
-        }
         delegate.commitTransaction(session);
         decrementTransaction(session);
     }
 
     public void rollbackTransaction(S session) {
-        if (getTransactionNesting(session) <= 0) {
-            flush();
-            return;
-        }
         delegate.rollbackTransaction(session);
         decrementTransaction(session);
     }
-    
-    private void decrementTransaction(S session) {
-        if (getTransactionNesting(session) == 0) {
-            readOrWrittenVarsMap.remove(session);
-            transactedReads.remove(session);
-        }
-    }
+   
 
     public int getTransactionNesting(S session) {
         return delegate.getTransactionNesting(session);
     }
 
     public void flush() {
-        myFlush();
+        readOrWrittenVarsMap.clear();
         delegate.flush();
     }
     
-    private void myFlush() {
-        readOrWrittenVarsMap.clear();
-        transactedReads.clear();
+    private void markReadOrWritten(S session, V var) {
+        Set<V> readOrWrittenVars = readOrWrittenVarsMap.get(session);
+        if (readOrWrittenVars != null) {
+            readOrWrittenVars.add(var);
+        }
+    }
+    
+    private void decrementTransaction(S session) {
+        if (getTransactionNesting(session) == 0) {
+            readOrWrittenVarsMap.remove(session);
+        }
     }
 }
