5520a3676189c411e834c85dde997dcafac5f11c
Tania Tudorache <tudorache@users.noreply.github.com>
2009-02-18 02:07:23 +0000

    adding the JVM arguments

--- bin/run_protege_server.bat
+++ bin/run_protege_server.bat
@@ -17,12 +17,17 @@ set HEADLESS=-Djava.awt.headless=true
 set CODEBASE=-Djava.rmi.server.codebase=%CODEBASE_URL%
 set LOG4J_OPT=-Dlog4j.configuration=file:log4j.xml
 
-set OPTIONS=%MAX_MEMORY% %CODEBASE% %HEADLESS% %LOG4J_OPT%
+rem --- Optional arguments; uncomment if necessary ---
+rem set "PORTOPTS=-Dprotege.rmi.server.port=5200 -Dprotege.rmi.registry.port=5100"
+rem TX=-Dtransaction.level=READ_COMMITTED
+rem "DEBUG_OPT=-Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n"
+
+set OPTIONS=%MAX_MEMORY% %CODEBASE% %HEADLESS% %LOG4J_OPT% %PORTOPTS% %TX% %DEBUG_OPT%
 
 rem ------------------- Cmd Options -------------------
 rem If you want automatic saving of the project, 
 rem setup the number of seconds in SAVE_INTERVAL_VALUE
-set SAVE_INTERVAL=-saveIntervalSec=120
+rem set SAVE_INTERVAL=-saveIntervalSec=120
 rem ------------------- Cmd Options -------------------
 
 
