696cebe7e69a8021dfa24ba0d169d828df00f10e
Tania Tudorache <tudorache@users.noreply.github.com>
2009-06-11 05:05:00 +0000

    fix for the ordering of class tree nodes, if quotes are present

--- src/edu/stanford/smi/protege/ui/ParentChildNodeComparator.java
+++ src/edu/stanford/smi/protege/ui/ParentChildNodeComparator.java
@@ -4,6 +4,7 @@ import java.util.Comparator;
 
 import edu.stanford.smi.protege.model.Frame;
 import edu.stanford.smi.protege.util.LazyTreeNode;
+import edu.stanford.smi.protege.util.StringUtilities;
 
 /**
  * Special comparator for Parent-Child tree (class-subclass tree), in which 
@@ -12,7 +13,8 @@ import edu.stanford.smi.protege.util.LazyTreeNode;
  *
  */
 public class ParentChildNodeComparator implements Comparator {
-	private static final char SPECIAL_NAME_CHAR = ':';
+	private static final char SPECIAL_NAME_CHAR_1 = ':';
+	private static final char SPECIAL_NAME_CHAR_2 = '\'';
 	
     public int compare(Object o1, Object o2) {
         if (o1 instanceof LazyTreeNode) {
@@ -39,13 +41,15 @@ public class ParentChildNodeComparator implements Comparator {
     };
     
     private static boolean isSpecialName(String s) {
-        return s.length() > 0 && s.charAt(0) == SPECIAL_NAME_CHAR;
+        return s.length() > 0 && s.charAt(0) == SPECIAL_NAME_CHAR_1;
     }
     
     private static int compareStrings(String s1, String s2) {
-        int result = s1.compareToIgnoreCase(s2);
+    	s1 = StringUtilities.unquote(s1);
+    	s2 = StringUtilities.unquote(s2);
+        int result = s1.compareToIgnoreCase(s2); 
         if (result == 0) {
-            result = s1.compareTo(s2);
+            result = s1.compareTo(s2);           
         }
         return result;
     }
