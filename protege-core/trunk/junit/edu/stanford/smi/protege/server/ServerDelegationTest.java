0301b73dc8ea8dc9246a4447ea525f35cb2df629
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2009-08-11 08:08:53 +0000

    using setDelegate

--- protege-core/trunk/junit/edu/stanford/smi/protege/server/ServerDelegationTest.java
+++ protege-core/trunk/junit/edu/stanford/smi/protege/server/ServerDelegationTest.java
@@ -6,10 +6,12 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
 import junit.framework.TestCase;
+import edu.stanford.smi.protege.model.Project;
 
 public class ServerDelegationTest extends TestCase {
     public static final String ADMIN_USER = "Admin";
     public static final String ADMIN_PASSWORD = "admin";
+    private RemoteServer server;
     
     static {
         Server_Test.setMetaProject("junit/pprj/policy/metaproject02.pprj");
@@ -19,34 +21,42 @@ public class ServerDelegationTest extends TestCase {
     protected void setUp() throws Exception {
         super.setUp();
         Server_Test.startServer();
+        server = (RemoteServer) Naming.lookup(Server_Test.NAME);
+        server.reinitialize();
     }
     
     public void testSuccessfulDelegation() throws MalformedURLException, RemoteException, NotBoundException {
         if (!Server_Test.isServerRunning()) {
             return;
         }
-        RemoteServer server = (RemoteServer) Naming.lookup(Server_Test.NAME);
         RemoteSession session = server.openSession(ADMIN_USER, "localhost", ADMIN_PASSWORD);
         assertTrue(session.allowDelegation());
         assertTrue(session.getUserName().equals(ADMIN_USER));
         assertTrue(session.getRealUserName().equals(ADMIN_USER));
-        RemoteSession delegate = session.makeDelegate(Server_Test.USER1);
-        assertTrue(delegate.getUserName().equals(Server_Test.USER1));
-        assertTrue(delegate.getRealUserName().equals(ADMIN_USER));
+
+        RemoteServerProject rp = server.openProject(Server_Test.PROJECT_NAME, session);
+        assertTrue(rp == null); // the important thing is that opening the project fails.
+
+        session.setDelegate(Server_Test.USER1);
+        assertTrue(session.getUserName().equals(Server_Test.USER1));
+        assertTrue(session.getRealUserName().equals(ADMIN_USER));
+        rp = server.openProject(Server_Test.PROJECT_NAME, session);
+        Project p = RemoteClientProject.createProject(server, rp, session, false);
+        assertTrue(p.getKnowledgeBase().getCls("Article").getInstanceCount() == 9);
+        p.dispose();
     }
     
     public void testFailedDelegation() throws MalformedURLException, RemoteException, NotBoundException {
         if (!Server_Test.isServerRunning()) {
             return;
         }
-        RemoteServer server = (RemoteServer) Naming.lookup(Server_Test.NAME);
         RemoteSession session = server.openSession(Server_Test.USER1, "localhost", Server_Test.PASSWORD1);
         assertTrue(!session.allowDelegation());
         assertTrue(session.getUserName().equals(Server_Test.USER1));
         assertTrue(session.getRealUserName().equals(Server_Test.USER1));
         boolean failed = false;
         try  {
-            session.makeDelegate(Server_Test.USER2);
+            session.setDelegate(Server_Test.USER2);
         }
         catch (IllegalAccessError iae) {
             failed = true;
