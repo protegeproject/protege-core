7f8dd711c13d8ab3474328706abbf15e1dda611c
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2009-12-15 20:44:26 +0000

    added property methods

--- protege-core/trunk/src/edu/stanford/smi/protege/server/metaproject/GroupOperation.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/server/metaproject/GroupOperation.java
@@ -4,13 +4,19 @@ import java.util.Collection;
 import java.util.Set;
 
 public interface GroupOperation {
-  Group getAllowedGroup();
+	Group getAllowedGroup();
 
-  Set<Operation> getAllowedOperations();
-  
-  void setAllowedGroup(Group group);
-  
-  void setAllowedOperations(Collection<Operation> operations);
-  
-  void addAllowedOperation(Operation operation);
+	Set<Operation> getAllowedOperations();
+
+	void setAllowedGroup(Group group);
+
+	void setAllowedOperations(Collection<Operation> operations);
+
+	void addAllowedOperation(Operation operation);
+
+	Collection<PropertyValue> getPropertyValues();
+
+	void setPropertyValues(Collection<PropertyValue> propertyValues);
+
+	void addPropertyValue(PropertyValue propertyValue);
 }
