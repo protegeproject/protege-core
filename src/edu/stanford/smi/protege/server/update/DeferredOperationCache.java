6631bcf6e68d21e6ac46d7f032d0bf2637bfe0ec
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2010-02-24 20:54:47 +0000

    DeferredTransactionCache -> DeferredOperationCache refactor

--- src/edu/stanford/smi/protege/server/update/DeferredOperationCache.java
+++ src/edu/stanford/smi/protege/server/update/DeferredOperationCache.java
@@ -7,63 +7,34 @@ import edu.stanford.smi.protege.server.RemoteSession;
 import edu.stanford.smi.protege.server.util.FifoReader;
 import edu.stanford.smi.protege.util.transaction.cache.Cache;
 import edu.stanford.smi.protege.util.transaction.cache.CacheResult;
-import edu.stanford.smi.protege.util.transaction.cache.serialize.CacheCommitTransaction;
-import edu.stanford.smi.protege.util.transaction.cache.serialize.CacheRollbackTransaction;
 import edu.stanford.smi.protege.util.transaction.cache.serialize.SerializedCacheUpdate;
 
 /**
- * This is a hacky cache designed to solve two problems seen by the remote client frame store.
- * Second, the begin/commit/rollback transaction and in some cases CacheDelete operations apply to all the frame caches 
- * in the RemoteClientFrameStore. It is clear that updating all of these caches on such an operation
+ * Certain operations, most noteably begin/commit/rollback transaction apply to all the cache's in the RemoteClientFrameStore.
+ * It is clear that updating all of these caches on such an operation
  * is expensive.  So we defer telling the frame caches about transaction operations until that cache is 
  * needed.  The RemoteClientFrameStore uses a Fifo writer to track the transactions that have not 
  * been seen by its various caches.  When this 
  * cache is needed it can read through all the transaction operations that it has not seen and it will be
  * ready to perform the needed operation.
  * <p/>
- * Second, it is sometimes necessary to start a cache when the invoker is already in a transaction.
- * This is bad.  It can lead to incorrect data in the untransacted cache and missing rollbacks.  The generic
- * more robust solution is implemented by the InvalidatableCache.  If this guys sees a rollback or commit
- * transaction that takes the transaction nesting to a negative number, it invalidates the cache.  This cache
- * can then be discarded later when it is noticed that the cache is bad.
- * <p/>
- * There is an alternative more delicate option that is perhaps unnecessary. Suppose that
- * <li> only one known session will be in a transaction at the time that the cache is created and the transaction 
- *      nesting is known at this time</li>
- * <li> no other session will be in a transaction at the time that the cache is created.</li>
- * I believe that these conditions are satisfied by the RemoteClientFrameStore.  In this case theDeferredTransactionsCache
- * can flush the cache when the transaction nesting gets for the session gets down to zero.
+ * Also, it is sometimes necessary to start a cache when the invoker is already in a transaction.
+ * This is bad.  It can lead to incorrect data in the untransacted cache and missing rollbacks.  This situation
+ * should be taken care of by the InvalidatableCache which commits suicide when it noties a transaction 
+ * with nesting less than 0.
  * 
  * @author tredmond
  *
  */
-public class DeferredTransactionsCache implements Cache<RemoteSession, Sft, List> {
+public class DeferredOperationCache implements Cache<RemoteSession, Sft, List> {
 	private FifoReader<SerializedCacheUpdate<RemoteSession, Sft, List>> transactionUpdates;
 	private Cache<RemoteSession, Sft, List> delegate;
 	
-	private RemoteSession invoker;
-	private boolean needsEndOfTransactionFlush = false;
-	
 	@SuppressWarnings("unchecked")
-    public DeferredTransactionsCache(Cache<RemoteSession, Sft, List> delegate, 
+    public DeferredOperationCache(Cache<RemoteSession, Sft, List> delegate, 
 	                                 FifoReader<SerializedCacheUpdate<RemoteSession, Sft, List>> transactionUpdates) {
-	    this(delegate, null, 0, transactionUpdates);
-	}
-	
-	@SuppressWarnings("unchecked")
-    public DeferredTransactionsCache(Cache<RemoteSession, Sft, List> delegate, 
-	                                 RemoteSession invoker,
-	                                 int invokersTransactionNesting,
-			                         FifoReader<SerializedCacheUpdate<RemoteSession, Sft, List>> transactionUpdates) {
 		this.delegate = delegate;
 		this.transactionUpdates = transactionUpdates;
-		this.invoker = invoker;
-		if (invokersTransactionNesting > 0) {
-		    needsEndOfTransactionFlush = true;
-		    while (invokersTransactionNesting-- > 0) {
-		        delegate.beginTransaction(invoker);
-		    }
-		}
 	}
 	
 	public CacheResult<List> readCache(RemoteSession session, Sft var) {
@@ -150,14 +121,6 @@ public class DeferredTransactionsCache implements Cache<RemoteSession, Sft, List
 		SerializedCacheUpdate<RemoteSession, Sft, List> update;
 		while ((update = transactionUpdates.read()) != null) {
 			update.performUpdate(delegate);
-			if (needsEndOfTransactionFlush &&
-			        (update instanceof CacheCommitTransaction
-			            || update instanceof CacheRollbackTransaction) &&
-		            invoker.equals(update.getSession()) &&
-		            getTransactionNesting(invoker) == 0) {
-			    delegate.flush();
-			    needsEndOfTransactionFlush = false;
-			}
 		}
 	}
 
