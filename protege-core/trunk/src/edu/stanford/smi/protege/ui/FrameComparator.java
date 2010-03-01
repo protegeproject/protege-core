95e4303ac9056aba6f4f7bbe0bb356ec6dda5095
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2010-03-01 01:43:45 +0000

    java 5

--- protege-core/trunk/src/edu/stanford/smi/protege/ui/FrameComparator.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/ui/FrameComparator.java
@@ -3,16 +3,15 @@ package edu.stanford.smi.protege.ui;
 import java.util.Comparator;
 
 import edu.stanford.smi.protege.model.Frame;
-import edu.stanford.smi.protege.util.Log;
 
 /**
  * Comparator for frames.  The actually comparison criterion is delegated to the Frame.compareTo() method.
  *
  * @author    Ray Fergerson <fergerson@smi.stanford.edu>
  */
-public class FrameComparator implements Comparator<Frame> {
+public class FrameComparator<X extends Frame> implements Comparator<X> {
 
-    public int compare(Frame f1, Frame f2) {
+    public int compare(X f1, X f2) {
         return  f1.compareTo(f2);
     }
 }
