d7ee7932a976229461acf2cd55b1998b3001b90f
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2010-06-12 06:34:42 +0000

    added caching to the rmi class loader

--- protege-core/trunk/src/edu/stanford/smi/protege/server/ProtegeRmiClassLoaderSpi.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/server/ProtegeRmiClassLoaderSpi.java
@@ -3,6 +3,7 @@ package edu.stanford.smi.protege.server;
 
 import java.net.*;
 import java.rmi.server.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 import edu.stanford.smi.protege.plugin.*;
 
@@ -12,16 +13,21 @@ import edu.stanford.smi.protege.plugin.*;
  * @author Ray Fergerson <fergerson@smi.stanford.edu>
  */
 public class ProtegeRmiClassLoaderSpi extends RMIClassLoaderSpi {
+	private ConcurrentHashMap<String, Class> cache = new ConcurrentHashMap<String, Class>();
 
     public ProtegeRmiClassLoaderSpi() {
     }
 
-    public Class loadClass(String codebase, String name, ClassLoader defaultLoader) throws MalformedURLException {
-        Class clas;
-        try {
-            clas = sun.rmi.server.LoaderHandler.loadClass(codebase, name, defaultLoader);
-        } catch (ClassNotFoundException e) {
-            clas = PluginUtilities.forName(name, true);
+    @SuppressWarnings("unchecked")
+	public Class loadClass(String codebase, String name, ClassLoader defaultLoader) throws MalformedURLException {
+        Class clas = cache.get(name);
+        if (clas == null) {
+        	try {
+        		clas = sun.rmi.server.LoaderHandler.loadClass(codebase, name, defaultLoader);
+        	} catch (ClassNotFoundException e) {
+        		clas = PluginUtilities.forName(name, true);
+        	}
+        	cache.putIfAbsent(name, clas);
         }
         return clas;
     }
