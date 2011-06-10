06b9f18374273511159d3da568487c7e582c3c4b
nyulas <2441046+csnyulas@users.noreply.github.com>
2011-06-10 07:46:29 +0000

    Added utility methods to create closures over an instance using an arbitrary property, AND methods to retrieve all values attached through a specific property to all the instances in such a closure.
    IMPORTANT: Before the next Protege release all these new methods need to be moved in the ModelUtilities.java class, and jUnit test needs to be activated.

--- junit/edu/stanford/smi/protege/model/ModelUtilities_Test.java
+++ junit/edu/stanford/smi/protege/model/ModelUtilities_Test.java
@@ -1,8 +1,9 @@
 package edu.stanford.smi.protege.model;
 
-import java.util.*;
+import java.util.Iterator;
+import java.util.List;
 
-import edu.stanford.smi.protege.test.*;
+import edu.stanford.smi.protege.test.APITestCase;
 
 /**
  * @author Holger Knublauch  <holger@smi.stanford.edu>
@@ -34,4 +35,97 @@ public class ModelUtilities_Test extends APITestCase {
         assertEquals(testCls, it.next());
         assertFalse(it.hasNext());
     }
+
+    /**
+     * :THING
+     *  |
+     *  +- R    p: n
+     *     |
+     *     +- Z    p: h
+     *     |  |
+     *     |  +- T    p: k
+     *     |  |
+     *     +--+- C    p: e
+     *     |     |
+     *     |     +- B    p: e
+     *     |     |
+     *     |     +- A    p: e
+     *     |        |
+     *     +--------+- Y    p: {n, ny, tk}
+     *                 |
+     *                 +- X    p: {o, h}
+     * ------------------------------------------------------
+     * Result should be: {tk=[y], h=[x, z], n=[y, r], ny=[y], o=[x], e=[c, a]}
+     * /
+    public void testGetPropertyValuesOnAllSuperclasses() {
+    	//create class hierarchy
+        Cls r = createCls();
+        Cls z = createSubCls(r);
+        Cls t = createSubCls(z);
+        Cls c = createSubCls(z);
+        c.addDirectSuperclass(r);
+        Cls b = createSubCls(c);
+        Cls a = createSubCls(c);
+        Cls y = createSubCls(a);
+        y.addDirectSuperclass(r);
+        Cls x = createSubCls(y);
+        
+        //create property values
+    	Slot p = createSlot();
+		r.addOwnSlotValue(p, "n");
+        z.addOwnSlotValue(p, "h");
+        t.addOwnSlotValue(p, "k");
+        c.addOwnSlotValue(p, "e");
+        b.addOwnSlotValue(p, "e");
+        a.addOwnSlotValue(p, "e");
+        y.addOwnSlotValue(p, "n");
+        y.addOwnSlotValue(p, "ny");
+        y.addOwnSlotValue(p, "tk");
+        x.addOwnSlotValue(p, "o");
+        x.addOwnSlotValue(p, "h");
+        
+        //test method
+        Map<Object, List<Instance>> valuesMap = ModelUtilities.getPropertyValuesOnAllSuperclasses(x, p);
+        //System.out.println(valuesMap);
+        
+        Set<Object> values = valuesMap.keySet();
+        //System.out.println(values);
+        
+        String[] expectedValues = new String[] {"n", "h", "e", "ny", "tk", "o"};
+        assertEquals(expectedValues.length, values.size());
+        for (String expValue : expectedValues) {
+			assertTrue(values.contains(expValue));
+		}
+        
+        List<Instance> occList;
+        occList = valuesMap.get("n");
+        assertEquals(2, occList.size());
+        assertTrue(occList.contains(r));
+        assertTrue(occList.contains(y));
+        
+        occList = valuesMap.get("h");
+        assertEquals(2, occList.size());
+        assertTrue(occList.contains(z));
+        assertTrue(occList.contains(x));
+        
+        occList = valuesMap.get("e");
+        assertEquals(2, occList.size());
+        assertTrue(occList.contains(a));
+        assertTrue(occList.contains(c));
+        
+        occList = valuesMap.get("ny");
+        assertEquals(1, occList.size());
+        assertTrue(occList.contains(y));
+        
+        occList = valuesMap.get("tk");
+        assertEquals(1, occList.size());
+        assertTrue(occList.contains(y));
+        
+        occList = valuesMap.get("o");
+        assertEquals(1, occList.size());
+        assertTrue(occList.contains(x));
+
+    }
+    /**/
+
 }
