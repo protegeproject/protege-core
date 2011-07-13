d0ce0bb279f98581df1f01250627f8130a72b8e3
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2011-07-13 23:16:51 +0000

    added some util methods for computing the closure on property values (moved from the ICD project, but they are generic)

--- protege-core/trunk/src/edu/stanford/smi/protege/model/ModelUtilities.java
+++ protege-core/trunk/src/edu/stanford/smi/protege/model/ModelUtilities.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import edu.stanford.smi.protege.util.Assert;
 import edu.stanford.smi.protege.util.CollectionUtilities;
@@ -21,8 +23,8 @@ import edu.stanford.smi.protege.util.Log;
  * should look up the needed frames once and then store them and use the stored
  * values. Nevertheless, for some rare or one-off operations these methods can
  * be useful and convenient.
- * 
- * 
+ *
+ *
  * @author Ray Fergerson <fergerson@smi.stanford.edu>
  */
 public class ModelUtilities {
@@ -115,6 +117,108 @@ public class ModelUtilities {
         return list;
     }
 
+    public static void getPropertyClosureToRoot(Instance resource, Slot parentSlot, Instance rootResource,
+            List<Instance> path, Collection<List<Instance>> pathLists) {
+        path.add(0, resource);
+
+        Collection<?> parents = resource.getOwnSlotValues(parentSlot);
+
+        for (Object parentObject : parents) {
+            if (parentObject instanceof Instance) {
+                Instance parentResource = (Instance) parentObject;
+                if (parentResource.equals(rootResource)) {
+                    List<Instance> copyPathList = new ArrayList<Instance>(path);
+                    copyPathList.add(0, parentResource);
+                    pathLists.add(copyPathList);
+                } else if (!path.contains(parentResource)) {
+                    //if (ModelUtilities.isVisibleInGUI(parentResource)) { //TODO: do we want this?
+                    List<Instance> copyPath = new ArrayList<Instance>(path);
+                    getPropertyClosureToRoot(parentResource, parentSlot, rootResource, copyPath, pathLists);
+                    //}
+                }
+            }
+        }
+    }
+
+    /**
+     * Computes all paths from an instance to the "root instance" node by navigating on a
+     * given slot.
+     *
+     * @param resource - an instance
+     * @param parentSlot - slot to navigate on towards a "root node"
+     * @param rootResource - an instance considered as the root of the navigation tree,
+     *              necessary to stop the navigation.
+     *
+     * @return a collection of the paths from the resource to the root resource
+     */
+    public static Collection<List<Instance>> getPropertyClosureToRoot(Instance resource, Slot parentSlot, Instance rootResource) {
+        Collection<List<Instance>> results = new ArrayList<List<Instance>>();
+        if (resource.equals(rootResource)) {
+            results.add(Collections.singletonList(rootResource));
+            return results;
+        }
+        getPropertyClosureToRoot(resource, parentSlot, rootResource, new LinkedList<Instance>(), results);
+        return results;
+    }
+
+    /**
+     * Returns all own slot values for the slot <code>slot</code> for every instance
+     * in any of the paths between an instance (<code>resource</code>) and
+     * a root instances (<code>rootResource</code>) following the relationships
+     * defined by the <code>parentSlot</code> slot.
+     *
+     * @param resource - a resource
+     * @param parentSlot - the slot that is used to traverse the instance graph to the <code>rootResource</code>
+     * @param rootResource - the resource that is the
+     * @param slot - a slot
+     *
+     * @return a map containing all own slot values as keys, and each value is mapped to a list
+     *      of instances which contained the value as their own slot value
+     */
+    public static Map<Object, List<Instance>> getPropertyValuesOnPropertyClosureToRoot(
+            Instance resource, Slot parentSlot, Instance rootResource, Slot slot) {
+        Map<Object, List<Instance>> result = new HashMap<Object, List<Instance>>();
+
+        Collection<List<Instance>> propertyClosureToRoot = getPropertyClosureToRoot(resource, parentSlot, rootResource);
+        Set<Instance> allNodes = new HashSet<Instance>();
+        for (List<Instance> path : propertyClosureToRoot) {
+            allNodes.addAll(path);
+        }
+
+        for (Instance node : allNodes) {
+            Collection<?> values = node.getOwnSlotValues(slot);
+            for (Object value : values) {
+                if (value != null) {
+                    List<Instance> nodeList = result.get(value);
+                    if (nodeList == null) {
+                        nodeList = new ArrayList<Instance>();
+                        result.put(value, nodeList);
+                    }
+                    nodeList.add(node);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Returns all own slot values for the slot <code>slot</code> for every superclass of a class.
+     *
+     * @param cls - a class
+     * @param slot - a slot
+     *
+     * @return a map containing all own slot values as keys, and each value is mapped to a list
+     *      of classes which contained the value as their own slot value
+     */
+    public static Map<Object, List<Instance>> getPropertyValuesOnAllSuperclasses(Cls cls, Slot slot) {
+        KnowledgeBase kb = cls.getKnowledgeBase();
+        Cls rootCls = kb.getRootCls();
+        Slot parentSlot = kb.getSystemFrames().getDirectSuperclassesSlot();
+
+        return getPropertyValuesOnPropertyClosureToRoot(cls, parentSlot, rootCls, slot);
+    }
+
     public static List<Cls> getPathToRoot(Cls cls) {
         return getPathToRoot(cls, new LinkedList<Cls>());
     }
@@ -145,10 +249,10 @@ public class ModelUtilities {
     /**
      * Computes all paths from a class to the root node by navigating on the
      * direct superclasses slot.
-     * 
+     *
      * @param cls
      *            - the resource
-     * 
+     *
      * @return a collection of the paths from the resource to the root
      */
     public static Collection<List<Cls>> getPathsToRoot(Cls cls) {
